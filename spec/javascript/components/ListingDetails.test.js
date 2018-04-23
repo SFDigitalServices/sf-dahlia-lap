d7863e7a80d2203f5845b95f91ae215aac2cf10e
Federico Dayan <federico.dayan@gmail.com>
2018-04-23 10:23:48 -0300

    adding Sinon for fake timer, so it does not break snapshots

--- spec/javascript/components/ListingDetails.test.js
+++ spec/javascript/components/ListingDetails.test.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import ListingDetails from 'components/ListingDetails'
+import sinon from 'sinon'
 
-test('Should render ListingDetails', () => {
-  /* More data is required here */
-  const listings = {
-    Listing_Lottery_Preferences: [],
-    Open_Houses: true,
-    Information_Sessions: [],
-    'Owner.Name': 'xxx'
-  }
-  const component = renderer.create(
-    <ListingDetails listing = { listings }/ >,
-  );
+describe('ListingDetails', () => {
+  let clock = null;
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  beforeEach(() => {
+    clock = sinon.useFakeTimers(new Date(2018, 3, 23).getTime());
+  });
+
+  afterEach(() => {
+    clock.restore();
+  })
+
+  test('Should render ListingDetails', () => {
+    /* More data is required here to be a real test*/
+    const listings = {
+      Listing_Lottery_Preferences: [],
+      Open_Houses: true,
+      Information_Sessions: [],
+      'Owner.Name': 'xxx'
+    }
+    const component = renderer.create(
+      <ListingDetails listing = { listings } />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 })
