bd079f24d8677381228f9180fca87e75923f7047
Federico Dayan <federico.dayan@gmail.com>
2018-04-26 16:47:06 -0300

    [#154784173] specs for Breadcrumbs and PageHeader

---
+++ spec/javascript/components/atoms/Breadcrumbs.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import BreadCrumbs from 'components/atoms/BreadCrumbs'
+import sinon from 'sinon'
+
+describe('BreadCrumbs', () => {
+  let items = [ { title: 'Alpha', link: '/alpah' }, { title: 'Beta', link: '/beta' }, { title: 'Gama', link: '/gama' }, { title: 'Pi', link: '/pi' }]
+
+  test('should render succesfully', () => {
+    const component = renderer.create(
+      <BreadCrumbs items={items} />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  test('should render succesfully without BreadCrumbs', () => {
+    const component = renderer.create(
+      <BreadCrumbs />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
