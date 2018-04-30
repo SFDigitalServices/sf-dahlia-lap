73ba2ec612df724a593c1e5948a250045d1f6ee7
slowbot <jesse@slowbot.com>
2018-04-30 10:13:31 -0700

    Fixing class to className

--- app/javascript/components/organisms/ContentSection.jsx
+++ app/javascript/components/organisms/ContentSection.jsx
@@ -3,12 +3,12 @@ import FormGridRow from '../molecules/FormGridRow'
 
 const ContentSection = ({ title, subtitle, label, id, name, placeholder, describeId, note, error }) => {
   return (
-    <div class="content-section">
-      <div class="app-inner header-wide">
-        <h3 class="app-card_h2">{title}</h3>
-        <p class="form-note max-width">{subtitle}</p>
+    <div className="content-section">
+      <div className="app-inner header-wide">
+        <h3 className="app-card_h2">{title}</h3>
+        <p className="form-note max-width">{subtitle}</p>
       </div>
-      <div class="app-inner inset-wide border-bottom">
+      <div className="app-inner inset-wide border-bottom">
         <FormGridRow label={label} id={id} name={name} placeholder={placeholder} describeId={describeId} note={note} error={error} />
       </div>
     </div>
--- app/javascript/components/organisms/TabCard.jsx
+++ app/javascript/components/organisms/TabCard.jsx
@@ -3,8 +3,8 @@ import ContentSection from '../organisms/ContentSection'
 
 const TabCard = ({ title, subtitle, label, id, name, placeholder, describeId, note, error }) => {
   return (
-    <section class="tabs-card-row row full-width inner--3x margin-bottom--2x">
-      <div class="app-card form-card tabs-card max-width expand-on-small">
+    <section className="tabs-card-row row full-width inner--3x margin-bottom--2x">
+      <div className="app-card form-card tabs-card max-width expand-on-small">
         <ContentSection title={title} subtitle={subtitle} label={label} id={id} name={name} placeholder={placeholder} describeId={describeId} note={note} error={error} />
       </div>
     </section>
