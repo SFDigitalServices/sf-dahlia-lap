a2b8c72063359ed1980414e2973bddd7fde4e09c
slowbot <jesse@slowbot.com>
2018-05-07 15:47:27 -0700

    Adding more detail for content sections

--- app/javascript/components/organisms/TabCard.jsx
+++ app/javascript/components/organisms/TabCard.jsx
@@ -5,7 +5,7 @@ const TabCard = ({ title, subtitle, title_description, subtitle_description, lab
   return (
     <section className="tabs-card-row row full-width inner--3x margin-bottom--2x">
       <div className="app-card form-card tabs-card max-width expand-on-small">
-        <ContentSection title={title} subtitle={subtitle} label={label} id={id} name={name} placeholder={placeholder} describeId={describeId} note={note} error={error} />
+        <ContentSection title={title} subtitle={subtitle} title_description={title_description} subtitle_description={subtitle_description} label={label} id={id} name={name} placeholder={placeholder} describeId={describeId} note={note} error={error} />
       </div>
     </section>
   )
