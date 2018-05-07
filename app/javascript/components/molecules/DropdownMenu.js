33b2fa98ad0bc2c499f5323ad991e5a958a09e34
Federico Dayan <federico.dayan@gmail.com>
2018-05-07 14:40:59 -0300

    [#154784997] fixing git: loading old code

--- app/javascript/components/molecules/DropdownMenu.js
+++ app/javascript/components/molecules/DropdownMenu.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import DropdownMenuItem from '../atoms/DropdownMenuItem'
 
-const DropdownMenu = ({ children }) => {
+const DropdownMenu = ({ items, value, onChange, style }) => {
   return (
-    <ul className="dropdown-menu" role="listbox" aria-hidden="true" aria-activedescendant tabIndex="-1">
-      {children}
+    <ul className="dropdown-menu" style={style} role="listbox" aria-hidden="true" aria-activedescendant tabindex="-1">
+      {
+        items.map((item) => (
+          <DropdownMenuItem
+            key={item.value}
+            {...item}
+            selected={item.value == value}
+            onChange={onChange} />)
+        )
+      }
     </ul>
   )
 }
