781f88ed182982b4c4b329553a6850f22ccf95ab
Federico Dayan <federico.dayan@gmail.com>
2018-05-07 17:30:12 -0300

    [#154784997] initial work on dropdown key navigation

--- app/javascript/components/atoms/DropdownMenuItem.js
+++ app/javascript/components/atoms/DropdownMenuItem.js
@@ -4,9 +4,19 @@ const DropdownMenuItem = ({ value, label, selected, onChange }) => {
   let liClassName = 'dropdown-menu_item'
   if (selected)
     liClassName += ' is-selected'
+
+  const onChangeHandler = () => {
+    onChange && onChange(value, label)
+  }
+
+  const onKeyPressHandler = (e) => {
+    onChangeHandler()
+    e.preventDefault()
+  }
+
   return (
     <li className={liClassName} role="option" aria-selected={selected}>
-      <a onClick={() => onChange && onChange(value, label) }>{label}</a>
+      <a tabIndex="0" onKeyPress={onKeyPressHandler} onClick={onChangeHandler}>{label}</a>
     </li>
   )
 }
