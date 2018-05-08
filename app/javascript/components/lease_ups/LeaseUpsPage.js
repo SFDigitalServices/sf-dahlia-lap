84909fcf723d63c339e8d9b223c558e830327627
Christine Dolendo <chris.dolendo@exygy.com>
2018-05-08 16:32:36 -0700

    [#154064861] - Removed wrapper for lease ups header

--- app/javascript/components/lease_ups/LeaseUpsPage.js
+++ app/javascript/components/lease_ups/LeaseUpsPage.js
@@ -6,7 +6,14 @@ import LeaseUpsTableContainer from './LeaseUpsTableContainer'
 const LeaseUpsPage = ({ listing, results }) => {
   return (
     <div>
-      <LeaseUpsHeader listing={listing} />
+    	<PageHeader
+    	  title={listing.Name}
+    	  content={listing.Building_Street_Address}
+    	  action={{ title: 'Export', link: `/listings/${listing.Id}/lease_ups/export` }}
+    	  breadcrumbs={[
+    	    { title: 'Lease Ups', link: '/lease_ups' },
+    	    { title: listing.Name, link: `/listings/${listing.Id}/lease_ups` }
+    	  ]}/>
       <LeaseUpsTableContainer listing={listing} results={results} />
     </div>
   )
