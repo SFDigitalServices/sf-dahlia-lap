93001c7f3057e758d5da98c05109efa95ae4a95a
Federico <fddayan@users.noreply.github.com>
2018-05-18 10:04:38 -0300

    Features/dropdown button component #154784997 (#18)
    * [#154784997] onChange handle and expanded implemented
    
    * [#154784997] implementing dropdown for multiple items
    
    * [#154784997] WIP: starting work dropdown multie select
    
    * [#154784997] making dropdown multi select and aligning menu to button
    
    * fixing git: file removed
    
    * [#154784997] fixing git: loading old code
    
    * [#154784997] yarn lock updated
    
    * [#154784997] snapshots updated
    
    * [#154784997] adding escape event
    
    * [#154784997] overwrite title for wrappers on PL index
    
    * [#154784997] items moved out of wrapper
    
    * [#154784997] snpashots updated
    
    * [#154784997] initial work on dropdown key navigation
    
    * [#154784997] adding tab navigation
    
    * [#154784997] cleanup code
    
    * [#154784997] fixing PL DropdownMenu and DropdownMenuMultiSelect previews
    
    * [#154784997] aria-hidden always false
    
    * [#154784997] removed aria-hidden

--- app/javascript/components/molecules/DropdownMenuMultiSelect.js
+++ app/javascript/components/molecules/DropdownMenuMultiSelect.js
@@ -1,12 +1,46 @@
 import React from 'react'
 import DropdownMenuItemCheckbox from '../atoms/DropdownMenuItemCheckbox'
+import _ from 'lodash'
 
-const DropdownMenuMultiSelect = ({ name, item, selected }) => {
-  return (
-    <ul className="dropdown-menu" role="listbox" aria-hidden="true" aria-multiselectable="true" aria-activedescendant tabIndex="-1">
-      <DropdownMenuItemCheckbox item={item} selected={selected} name={name}/>
-    </ul>
-  )
+class DropdownMenuMultiSelect extends React.Component {
+
+  // I chose to keep the state out of the component,
+  // so we might be able to integrate it with mobx
+  // Fed
+  onChangeHandler = (value, label, selected) => {
+    let newValues = []
+
+    // Add original values
+    if (this.props.values)
+      newValues = newValues.concat(this.props.values)
+
+    if (selected) // add new value if selected
+      newValues.push(value)
+    else          //remove value if unslected
+      newValues = _.remove(newValues, v => v != value)
+
+    const uniqValues = _.uniq(newValues) // remove duplications
+
+    this.props.onChange(uniqValues)
+  }
+
+  render() {
+    const { items, values, onChange, style } = this.props
+    return (
+      <ul className="dropdown-menu" style={style} role="listbox" aria-multiselectable="true" aria-activedescendant tabindex="-1">
+        {
+          items &&
+          items.map((item) => (
+            <DropdownMenuItemCheckbox
+              key={item.value}
+              {...item}
+              selected={_.includes(values, item.value)}
+              onChange={this.onChangeHandler} />
+          ))
+        }
+      </ul>
+    )
+  }
 }
 
 export default DropdownMenuMultiSelect
