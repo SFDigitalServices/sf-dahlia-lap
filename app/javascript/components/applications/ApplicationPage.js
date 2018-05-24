4fcc892d667162be41ad7e8f8de1787380d3d24d
Federico Dayan <federico.dayan@gmail.com>
2018-05-24 09:41:40 -0400

    [#157673726] Application page with layout

--- app/javascript/components/applications/ApplicationPage.js
+++ app/javascript/components/applications/ApplicationPage.js
@@ -1,19 +1,7 @@
 import React from 'react'
 
-// import PageHeader from '../organisms/PageHeader'
 import ApplicationDetails from './ApplicationDetails'
 import CardLayout from '../layouts/CardLayout'
-// import mapProps from '../../utils/mapProps'
-
-// const ApplicationPageHeader = ({ application }) => {
-//   return (
-//     <div>
-//       <PageHeader
-//         title={`Application ${application.Name}`}
-//         content={<span>Name of Listing: <a href={`/listings/${application.Listing.Id}`}>{application.Listing.Name}</a></span>} />
-//     </div>
-//   )
-// }
 
 const ApplicationPage = (props) => {
   const { application } = props
@@ -30,12 +18,5 @@ const ApplicationPage = (props) => {
     </CardLayout>
   )
 }
-
-// const mapProperties = ({application, statusHistory}) => {
-//   return {
-//     application: mapApplicationProperty(application),
-//     statusHistory: mapStatusHistoryProperty(statusHistory)
-//   }
-// }
 
 export default ApplicationPage
