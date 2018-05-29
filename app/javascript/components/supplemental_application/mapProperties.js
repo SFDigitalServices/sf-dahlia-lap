cc26a99ba70f7c7b7a18a078d618aa61304df2f9
Federico <fddayan@users.noreply.github.com>
2018-05-29 13:15:03 -0400

    Features/single lease up status history #154072850 (#25)
    * [#154785586] adding json viewer for props to make it easier to read
    
    * [#154785586] using json tree
    
    * [#154785586] rename to currentUrl
    
    * [#154785586] adding cycle
    
    * [#154785586] cleanup
    
    * [#154785586] snapshot updated
    
    * [#154785586] change tab on PL view, for easier review
    
    * [#154785586] cleanup React JSX warnings
    
    * [#154785586] setting tab index -1 on active tab so we skip
    
    * [#154785586] adding specs to TabMenu
    
    * [#154785586] adding specs for TabSection
    
    * [#154785586] adding one more tab for easier review
    
    * [#154785586] keyboard event helper introduced
    
    * [#154785586]  arrayutils renamed
    
    * [#154785586] making tabs and keys play nice together
    
    * #155206093 init suppl app page
    
    * [#154785586] ContentSection moved out of TabCard
    
    * [#155206093] page header and tab section added
    
    * [#155206093] tabfixed and snpashot updated
    
    * [#155206093] adding links
    
    * [#155206093] loading supplemental application info from salesforce
    
    * [#155206093] controller refactored
    
    * [#155206093] missing merge
    
    * [#154072850] loading status history from Salesforce
    
    * [#154072850] cleanup
    
    * [#154072850] sort and pretty time
    
    * [#154072850] refactored status to use plain from saleforce
    
    * [#154072850] jest test added
    
    * [#154072850] cleanup
    
    * [#154072850] content section spec added
    
    * [#154072850] console log removed
    
    * [#154072850] status updated, and time reversed
    
    * [#154072850] using toLower
    
    * [#154072850] snapshot updated
    
    * [#155206093] supppl app header using right values from listing. Cleanup
    
    * [#154072850] snpashot updated
    
    * [#154072850] AppInner removed
    
    * [#154072850] cleanup
    
    * [#154072850] specs fixed
    
    * [#157673726] fixed date sort and date format for status list
    
    * [#154072850] snapshot updated
    
    * [#154072850] map Lease Signed to is-leased class name

---
+++ app/javascript/components/supplemental_application/mapProperties.js
@@ -0,0 +1,42 @@
+import _ from 'lodash'
+import moment from 'moment'
+
+const mapListing = (listing) => {
+  return {
+    id: listing.Id,
+    name: listing.Name
+  }
+}
+
+const mapApplicationProperty = (application) => {
+  return {
+    id: application.Id,
+    number: application.Name,
+    name: application.Applicant.Name,
+    listing: mapListing(application.Listing)
+  }
+}
+
+const mapStatusHistoryItem = (item) => {
+  return {
+    status: item.Processing_Status,
+    note: item.Processing_Comment,
+    date: item.Processing_Date_Updated,
+    timestamp: moment(item.Processing_Date_Updated).unix()
+  }
+}
+
+const mapStatusHistoryProperty = (statusHistory) => {
+  if (_.isEmpty(statusHistory))
+    return []
+  return statusHistory.map(mapStatusHistoryItem)
+}
+
+const mapProperties = ({application, statusHistory}) => {
+  return {
+    application: mapApplicationProperty(application),
+    statusHistory: mapStatusHistoryProperty(statusHistory)
+  }
+}
+
+export default mapProperties
---
+++ app/javascript/utils/mapProps.js
@@ -0,0 +1,9 @@
+import { createFactory } from 'react'
+
+const mapProps = propsMapper => BaseComponent => {
+  const factory = createFactory(BaseComponent)
+  const MapProps = props => factory(propsMapper(props))
+  return MapProps
+}
+
+export default mapProps
