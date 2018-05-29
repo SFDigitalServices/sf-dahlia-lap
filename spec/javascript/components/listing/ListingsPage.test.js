9f6215dacf6cfbe24a170780376177457f98ec05
Federico Dayan <federico.dayan@gmail.com>
2018-05-29 11:03:08 -0400

    [#157673726] snashots updated

--- spec/javascript/components/listing/ListingsPage.test.js
+++ spec/javascript/components/listing/ListingsPage.test.js
@@ -3,8 +3,10 @@ import renderer from 'react-test-renderer';
 
 import ListingPage from 'components/listings/ListingPage'
 import modelsFactory from '../../factories/models'
+import sharedHooks from '../../support/sharedHooks'
 
 describe('ListingsPage', () => {
+  sharedHooks.useFakeTimers()
   test('should render succesfully', () => {
     const listing = modelsFactory.listing(1)
 
