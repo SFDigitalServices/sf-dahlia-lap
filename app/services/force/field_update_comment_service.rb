e73293810923cc929acb0352ac4688d1500b491e
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-06-01 12:43:26 -0700

    [#154064901] Adds error trigger on status update for testing

--- app/services/force/field_update_comment_service.rb
+++ app/services/force/field_update_comment_service.rb
@@ -7,7 +7,12 @@ module Force
       formatted_datetime = Time.now.strftime('%Y-%m-%dT%H:%M:%S%z')
       data = data.merge(Processing_Date_Updated__c: formatted_datetime)
       data = Hashie::Mash.new(data)
-      @client.create!('Field_Update_Comment__c', data)
+      # TODO: remove this error trigger that is here for testing purposes
+      if data[:Application__c] == 'a0o0P00000AxBWAQA3'
+        @client.create!('Field_Update_Comment__cfoofoofoo', data)
+      else
+        @client.create!('Field_Update_Comment__c', data)
+      end
     end
 
     def status_history_by_application(application_id)
