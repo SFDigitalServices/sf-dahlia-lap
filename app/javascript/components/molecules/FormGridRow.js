1b1a681400c8557333b5cb5490b7a766e16edcdc
Federico Dayan <federico.dayan@gmail.com>
2018-06-14 10:59:48 -0300

    [#155196549] removed duplicated code

--- app/javascript/components/molecules/FormGridRow.js
+++ app/javascript/components/molecules/FormGridRow.js
@@ -11,24 +11,4 @@ const FormGridRow = ({ label, id, name, placeholder, describeId, note, error }) 
   )
 }
 
-const FormGrid = {}
-
-FormGrid.Row = ({children}) => (
-  <div className="form-grid row expand padding-bottom">
-    {children}
-  </div>
-)
-
-FormGrid.Item = ({children}) => (
-  <div className="form-grid_item small-12 medium-6 large-3 column">
-    {children}
-  </div>
-)
-
-FormGrid.Group = ({children, label}) => (
-  <div className="form-group">
-    {children}
-  </div>
-)
-
 export default FormGridRow
