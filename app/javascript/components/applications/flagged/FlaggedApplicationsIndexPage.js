7e0614b782c309d691b6f87f725e0593bec6d6f5
Federico Dayan <federico.dayan@gmail.com>
2018-06-15 14:04:33 -0300

    [#154747783] Prop mapper added for FlaggedApplicationsIndexPage

--- app/javascript/components/applications/flagged/FlaggedApplicationsIndexPage.js
+++ app/javascript/components/applications/flagged/FlaggedApplicationsIndexPage.js
@@ -3,23 +3,29 @@ import React from 'react'
 import IndexTable from '../../IndexTable'
 import TableLayout from '../../layouts/TableLayout'
 
-const FlaggedApplicationsIndexPageTable = ({ results, fields }) => {
-  return (
-    <div>
-      <IndexTable
-        results={results}
-        fields={fields}
-        links={['View Flagged Applications']} />
-    </div>
-  )
+import mapProps from '~/utils/mapProps'
+
+const FlaggedApplicationsIndexTable = ({ results, fields }) => {
+  return <IndexTable
+          results={results}
+          fields={fields}
+          links={['View Flagged Applications']} />
 }
 
-const FlaggedApplicationsIndexPage = ({title, ...props}) => {
+const FlaggedApplicationsIndexPage = ({title, results, fields}) => {
   return (
     <TableLayout pageHeader={{title: title}}>
-      <FlaggedApplicationsIndexPageTable {...props} />
+      <FlaggedApplicationsIndexTable results={results} fields={fields} />
     </TableLayout>
   )
 }
 
-export default FlaggedApplicationsIndexPage
+const mapProperties = ({title, results, fields }) => {
+  return {
+    title,
+    results, // TODO: map here
+    fields
+  }
+}
+
+export default mapProps(mapProperties)(FlaggedApplicationsIndexPage)
