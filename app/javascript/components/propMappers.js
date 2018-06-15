a8bd8ea53bc276a64ca4e7efcb052daf672127fe
Federico Dayan <federico.dayan@gmail.com>
2018-06-15 11:39:49 -0300

    [#154747783] Prop mapping for LeaseUpPage

--- app/javascript/components/propMappers.js
+++ app/javascript/components/propMappers.js
@@ -5,7 +5,8 @@ export const mapListing = (listing) => {
   else {
     return {
       id: listing.Id,
-      name: listing.Name
+      name: listing.Name,
+      building_street_address: listing.Building_Street_Address
     }
   }
 }
@@ -18,5 +19,24 @@ export const mapApplication = (application) => {
     isLotterComplete: application.Is_Lottery_Complete,
     submissionType: application.Application_Submission_Type,
     listing: mapListing(application.Listing)
+  }
+}
+
+export const mapLeaseUpApplication = (application) => {
+  return {
+    id: application['Application'],
+    application_number: application['Application.Name'],
+    first_name: application['Application.First_Name'],
+    last_name: application['Application.Last_Name'],
+    phone: application['Application.Phone'],
+    email: application['Application.Email'],
+    status_updated: application['Status_Last_Updated'],
+    lease_up_status: application['Application.Processing_Status'],
+    preference_order: application['Preference_Order'],
+    mailing_address: application['Application.Mailing_Address'],
+    residence_address: application['Application.Residence_Address'],
+    preference_record_type: application['Listing_Preference_ID.Record_Type_For_App_Preferences'],
+    preference_lottery_rank: application['Preference_Lottery_Rank'],
+    preference_order:  application['Preference_Order']
   }
 }
