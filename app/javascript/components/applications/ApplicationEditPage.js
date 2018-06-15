302d665f24012ac2de3923d4c710349036329ecc
Federico Dayan <federico.dayan@gmail.com>
2018-06-15 13:22:29 -0300

    [#154747783] Prop mapper added for ApplicationEditPage

--- app/javascript/components/applications/ApplicationEditPage.js
+++ app/javascript/components/applications/ApplicationEditPage.js
@@ -3,6 +3,8 @@ import React from 'react'
 import PaperApplicationForm from './application_form/PaperApplicationForm'
 import CardLayout from '../layouts/CardLayout'
 
+import mapProps from '~/utils/mapProps'
+
 const ApplicationEditPageForm = ({ listing, application, editPage }) => {
   return (
     <PaperApplicationForm
@@ -12,8 +14,7 @@ const ApplicationEditPageForm = ({ listing, application, editPage }) => {
   )
 }
 
-const ApplicationEditPage = (props) => {
-  const { application, listing } = props
+const ApplicationEditPage = ({ listing, application, editPage }) => {
   const pageHeader = {
     title: 'Edit Application',
     content: `Application lottery number: ${application.Lottery_Number}. For listing: ${listing.Name}`
@@ -21,9 +22,20 @@ const ApplicationEditPage = (props) => {
 
   return (
     <CardLayout pageHeader={pageHeader}>
-      <ApplicationEditPageForm {...props} />
+      <ApplicationEditPageForm
+        listing={listing}
+        application={application}
+        editPage={editPage} />
     </CardLayout>
   )
 }
 
-export default ApplicationEditPage
+const mapProperties = ({ listing, application, editPage }) => {
+  return {
+    listing: listing,         // TODO: map here
+    application: application, // TODO: map here
+    editPage
+  }
+}
+
+export default mapProps(mapProperties)(ApplicationEditPage)
