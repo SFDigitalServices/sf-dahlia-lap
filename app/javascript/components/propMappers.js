f2a1b294f97d160e67bbbde5e9d1dd2a29cc4e32
Federico Dayan <federico.dayan@gmail.com>
2018-06-17 12:42:44 -0300

    [#154747783] Mappings added for ListingDetails WIP

--- app/javascript/components/propMappers.js
+++ app/javascript/components/propMappers.js
@@ -42,30 +42,109 @@ export const mapLeaseUpApplication = (application) => {
 }
 
 export const mapListingDetails = (listing) => {
+  // return listing
   return {
-    owner_name: 'Owner.Name',
-    name: 'Name',
-    account_name: 'Account.Name',
-    application_due_date: 'Application_Due_Date',
-    in_lottery: 'In_Lottery',
-    status: 'Status',
-    lottery_winners: 'Lottery_Winners',
-    lottery_results: 'Lottery_Results',
-    building_name: 'Building_Name',
-    building_street_address: 'Building_Street_Address',
-    building_city: 'Building_City',
-    building_state: 'Building_State',
-    building_zip_code: 'Building_Zip_Code',
-    neighborhood: 'Neighborhood',
-    developer: 'Developer',
-    building_url: 'Building_URL',
-    year_built: 'Year_Built',
-    description: 'Description',
-    lottery_preferences: 'Lottery_Preferences',
-    lottery_preference_name: 'Lottery_Preference.Name'
-    // 'Description',
-    // 'PDF_URL',
-    // 'Order',
-    // 'Available_Units'
+    id:listing.Id,
+    owner_id: listing.OwnerId,
+    owner: {
+      name: listing.Owner.Name
+    },
+    application_due_date: listing.Application_Due_Date,
+    name: listing.Name,
+    status: listing.Status,
+    building: { name : listing.Building.Name },
+    min_br: listing.Min_BR,
+    lotterry_winners: listing.Lottery_Winners,
+    max_br: listing.Max_BR,
+    lottery_results: listing.Lottery_Results,
+    min_income: listing.Min_Income,
+    account: {
+      name: listing.Account.Name
+    },
+    max_income: listing.Max_Income,
+    min_occupancy: listing.Min_Occupancy,
+    max_occupancy: listing.Max_Occupancy,
+    building_name: listing.Building_Name,
+    neighborhood: listing.Neighborhood,
+    building_street_address: listing.Building_Street_Address,
+    developer: listing.Developer,
+    building_city: listing.Building_City,
+    building_url: listing.Building_URL,
+    building_state: listing.Building_State,
+    year_built: listing.Year_Built,
+    building_zip_code: listing.Building_Zip_Code,
+    description: listing.Description,
+    lottery_prefrences: listing.Lottery_Preferences,
+    acessibility: listing.Accessibility,
+    fee: listing.Fee,
+    amenities: listing.Amenities,
+    deposit_min: listing.Deposit_Min,
+    costs_not_included: listing.Costs_Not_Included,
+    deposit_max: listing.Deposit_Max,
+    lottery_date: listing.Lottery_Date,
+    lottery_results_date: listing.Lottery_Results_Date,
+    lottery_venue: listing.Lottery_Venue,
+    lottery_summary: listing.Lottery_Summary,
+    lottery_street_address: listing.Lottery_Street_Address,
+    lottery_city: listing.lottery_city,
+    lottery_url: listing.Lottery_URL,
+    reserved_community_type: listing.Reserved_community_type,
+    application_phone: listing.Application_Phone,
+    application_organization: listing.Application_Organization,
+    application_street_address: listing.Application_Street_Address,
+    application_city: listing.Application_City,
+    download_url: listing.Download_URL,
+    application_sate: listing.Application_State,
+    organization_url: listing.Organization_URL,
+    application_postal_code: listing.Application_Postal_Code,
+    in_lottery: listing.In_Lottery,
+    leasing_agent_name: listing.Leasing_Agent_Name,
+    Leasing_Agent_title: listing.Leasing_Agent_Title,
+    leasing_agent_email: listing.Leasing_Agent_Email,
+    leasing_agent_phone: listing.Leasing_Agent_Phone,
+    legal_disclaimers: listing.Legal_Disclaimers,
+    building_selection_criteria: listing.Building_Selection_Criteria,
+    pet_policy: listing.Pet_Policy,
+    required_documents: listing.Required_Documents,
+    smoking_policy: listing.Smoking_Policy,
+    eviction_history: listing.Eviction_History,
+    criminal_history: listing.Criminal_History,
+    credit_rating: listing.Credit_Rating,
+    lottery_status: listing.Lottery_Status,
+    office_hours: listing.Office_Hours,
+    information_sessions: listing.Information_Sessions,
+    open_houses: listing.Open_Houses,
+    listing_lottery_preference: listing.Listing_Lottery_Preferences.map(mapListingLotteryPreferece),
+    units: listing.Units.map(mapUnit)
+  }
+}
+
+
+const mapListingLotteryPreferece = (listingLotteryPreference) => {
+  return {
+    id:listingLotteryPreference.Id,
+    total_submitted_apps:listingLotteryPreference.Total_Submitted_Apps,
+    order:listingLotteryPreference.Order,
+    description:listingLotteryPreference.Description,
+    available_units:listingLotteryPreference.Available_Units,
+    pdf_url:listingLotteryPreference.PDF_URL,
+    lottery_preference: {
+      id: listingLotteryPreference.Lottery_Preference.Id,
+      name: listingLotteryPreference.Lottery_Preference.Name
+    }
+  }
+}
+
+const mapUnit = (unit) => {
+  return {
+    unit_type: unit.Unit_Type,
+    bmr_rent_monthly: unit.BMR_Rent_Monthly,
+    bmr_rental_minimum_monthly_income_needed: unit.BMR_Rental_Minimum_Monthly_Income_Needed,
+    status: unit.Status,
+    property_type: unit.Property_Type,
+    ami_chart_type: unit.AMI_chart_type,
+    ami_chart_year: unit.AMI_chart_year,
+    of_ami_for_pricing_unit: unit.of_AMI_for_Pricing_Unit,
+    reserved_type: unit.Reserved_Type,
   }
 }
