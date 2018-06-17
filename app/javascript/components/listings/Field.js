3ad072b0016571c0019dbfd36fac5fef93cedfb3
Federico Dayan <federico.dayan@gmail.com>
2018-06-17 11:03:42 -0300

    [#154747783] Refactor fields

---
+++ app/javascript/components/listings/Field.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+var generateHtml = (value) => {
+  return {__html: value}
+}
+
+const RenderType = ({type, value}) => {
+  if (type === 'html') {
+    return (<p dangerouslySetInnerHTML={generateHtml(value)} />)
+  } else if (type === 'link') {
+    return(<a target='_blank' href={value}>{value}</a>)
+  } else {
+    return (<p>{String(value)}</p>)
+  }
+}
+
+const Content = ({ label, value, type }) => {
+  return (
+    <div className="margin-bottom--half">
+      <h4 className="t-sans t-small t-bold no-margin">
+        {label}
+      </h4>
+      <RenderType type={type} value={value}/>
+    </div>
+  )
+}
+
+
+export default Content
--- app/javascript/components/listings/generateContent.js
+++ app/javascript/components/listings/generateContent.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { includes, take, takeRight, map, isString } from 'lodash'
+import { includes, isString } from 'lodash'
 import moment from 'moment'
-import utils from '~/utils/utils'
 
-var generateHtml = (value) => {
-  return {__html: value}
-}
+import utils from '~/utils/utils'
+import Field from './Field'
 
 const getFormatType = (field) => {
   if (includes(field, 'Date'))
@@ -23,16 +21,16 @@ const getRenderType = (value) => {
 }
 
 const getTypedValue = (value, type) => {
-  if (type == 'date')
+  if (type === 'date')
     return moment(value).format('L')
   else
     return value
 }
 
-const getLabelValue = (listing, entry) => {
+export const buildFieldEntry = (listing, entry) => {
   let value = listing[entry.field]
   let label = utils.cleanField(entry.label)
-  let renderType = getRenderType(value)
+  let renderType = entry.renderType || getRenderType(value)
 
   if (includes(entry.field, '.')) {
     let parts = entry.field.split('.')
@@ -47,7 +45,7 @@ const getLabelValue = (listing, entry) => {
   return { label, value, renderType }
 }
 
-const buildFielEntryWithDefaults = (entry) => {
+export const buildFieldSpecs = (entry) => {
   let entryWithDefaults = isString(entry) ? { field: entry, label: entry } : entry
 
   if (!entryWithDefaults.label)
@@ -59,35 +57,13 @@ const buildFielEntryWithDefaults = (entry) => {
   return entryWithDefaults
 }
 
-const RenderType = ({type, value}) => {
-  if (type == 'html') {
-    return (<p dangerouslySetInnerHTML={generateHtml(value)} />)
-  } else if (type == 'link') {
-    return(<a target='_blank' href={value}>{value}</a>)
-  } else {
-    return (<p>{String(value)}</p>)
-  }
-}
-
-const Content = ({ label, value, type }) => {
-  return (
-    <div className="margin-bottom--half">
-      <h4 className="t-sans t-small t-bold no-margin">
-        {label}
-      </h4>
-      <RenderType type={type} value={value}/>
-    </div>
-  )
-}
-
-export const generateContent = (listing, field, i) => {
-  const entry = buildFielEntryWithDefaults(field)
-  const { label, value, renderType } = getLabelValue(listing, entry)
+export const generateContent = (listing, entry, i) => {
+  const { label, value, renderType } = entry
   if (!value)
     return null
   else
-    return <Content key={i}
+    return <Field key={i}
                     label={label}
                     value={value}
-                    type={entry.renderType || renderType} />
+                    type={renderType} />
 }
