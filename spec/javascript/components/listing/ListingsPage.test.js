7caae18deea75f318fcaced7471f5f246f3dd3ef
Federico Dayan <federico.dayan@gmail.com>
2018-06-18 16:04:00 -0300

    [#154747783] adding spec for ListingPage

--- spec/javascript/components/listing/ListingsPage.test.js
+++ spec/javascript/components/listing/ListingsPage.test.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 
 import ListingsPage from 'components/listings/ListingsPage'
 import modelsFactory from '../../factories/models'
+import listings from '../../fixtures/listings'
 
 describe('ListingsPage', () => {
   test('should render succesfully', () => {
@@ -10,7 +11,17 @@ describe('ListingsPage', () => {
     const fields = modelsFactory.listingFields()
 
     const wrapper = renderer.create(
-      <ListingsPage results={results} fields={fields} />,
+      <ListingsPage listings={results} fields={fields} />,
+    )
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  })
+
+
+  test('should render succesfully long list', () => {
+    const fields = modelsFactory.listingFields()
+    const wrapper = renderer.create(
+      <ListingsPage listings={listings} fields={fields} />,
     )
 
     expect(wrapper.toJSON()).toMatchSnapshot();
