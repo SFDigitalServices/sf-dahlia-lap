e4b298be46dde4b994213995a6905abee8da272d
Federico Dayan <federico.dayan@gmail.com>
2018-06-19 14:38:20 -0300

    [#154747783] Adding Fixture for FlaggedApplicationsShowPage.test.js

--- app/javascript/components/mappers/soqlToDomain.js
+++ app/javascript/components/mappers/soqlToDomain.js
@@ -189,3 +189,16 @@ export const mapFlaggedRecords = (flaggedRecord) => {
     total_number_of_appealed: flaggedRecord.Total_Number_of_Appealed,
   }
 }
+
+export const mapFlaggedApplication = (flaggedApplication) => {
+  return {
+    id: flaggedApplication.Id,
+    application: flaggedApplication.Application,
+    application_name: flaggedApplication['Application.Name'] ,
+    flagged_record_set_rule_name: flaggedApplication['Flagged_Record_Set.Rule_Name'],
+    primary_application_applicant_name: flaggedApplication['Primary_Application_Applicant_Name'],
+    flagged_Record_set_listing_lottery_status: flaggedApplication["Flagged_Record_Set.Listing.Lottery_Status"],
+    review_status: flaggedApplication["Review_Status"],
+    comments: flaggedApplication['Comments']
+  }
+}
---
+++ spec/javascript/fixtures/flagged_applications.js
@@ -0,0 +1,44 @@
+const flaggedApplications = [
+  {
+    "Id": "a0q0x000000N0UVAA0",
+    "Application": "a0o0P00000FEUwHQAX",
+    "Application.Name": "APP-00038758",
+    "Flagged_Record_Set.Rule_Name": "Name + DOB",
+    "Primary_Application_Applicant_Name": "Christine Dolendo",
+    "Flagged_Record_Set.Listing.Lottery_Status": null,
+    "Review_Status": "Pending Review",
+    "Comments": null
+  },
+  {
+    "Id": "a0q0x000000N0UWAA0",
+    "Application": "a0o0x0000003mRYAAY",
+    "Application.Name": "APP-00185173",
+    "Flagged_Record_Set.Rule_Name": "Name + DOB",
+    "Primary_Application_Applicant_Name": "Christine Dolendo",
+    "Flagged_Record_Set.Listing.Lottery_Status": null,
+    "Review_Status": "Pending Review",
+    "Comments": null
+  },
+  {
+    "Id": "a0q0x000000N0blAAC",
+    "Application": "a0o0x0000003n8PAAQ",
+    "Application.Name": "APP-00185369",
+    "Flagged_Record_Set.Rule_Name": "Name + DOB",
+    "Primary_Application_Applicant_Name": "Christine Dolendo",
+    "Flagged_Record_Set.Listing.Lottery_Status": null,
+    "Review_Status": "Pending Review",
+    "Comments": null
+  },
+  {
+    "Id": "a0q0x000000N0bqAAC",
+    "Application": "a0o0x0000003n8UAAQ",
+    "Application.Name": "APP-00185370",
+    "Flagged_Record_Set.Rule_Name": "Name + DOB",
+    "Primary_Application_Applicant_Name": "Christine Dolendo",
+    "Flagged_Record_Set.Listing.Lottery_Status": null,
+    "Review_Status": "Pending Review",
+    "Comments": null
+  }
+]
+
+export default flaggedApplications
