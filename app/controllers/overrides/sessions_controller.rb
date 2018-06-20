5eeaf8aca73c7e5a8c0562937facd354c53a7622
Grant Kinney <grant@exygy.com>
2018-06-20 10:33:47 -0500

    [#157983608] Improves salesforce sign out
    Call native devise signout path if Salesforce url is not available for some reason.

--- app/controllers/overrides/sessions_controller.rb
+++ app/controllers/overrides/sessions_controller.rb
@@ -2,13 +2,17 @@ module Overrides
   # Overrides to DeviseTokenAuth
   class SessionsController < Devise::SessionsController
     def destroy
-      @logout_path = current_user.admin ? ENV['SALESFORCE_INSTANCE_URL'] : ENV['COMMUNITY_LOGIN_URL']
+      @salesforce_logout_host = current_user.admin ? ENV['SALESFORCE_INSTANCE_URL'] : ENV['COMMUNITY_LOGIN_URL']
       Force::Base.new(current_user).revoke_token
       super
     end
 
     def after_sign_out_path_for(resource_or_scope)
-      "#{@logout_path}/secur/logout.jsp"
+      if @salesforce_logout_host
+        "#{@salesforce_logout_host}/secur/logout.jsp"
+      else
+        super(resource_or_scope)
+      end
     end
   end
 end
