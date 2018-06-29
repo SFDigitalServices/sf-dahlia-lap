0639896420b192f77d7464ea8347833e5e6ed078
Federico <federico.dayan@gmail.com>
2018-06-29 11:03:59 -0300

    [#154747783] Cleanups

--- app/javascript/components/supplemental_application/actions.js
+++ app/javascript/components/supplemental_application/actions.js
@@ -2,8 +2,6 @@ import apiService from '~/apiService'
 import domainToApi from '~/components/mappers/domainToApi'
 import Alerts from '~/components/Alerts'
 
-
-
 export const updateApplicationAction = async (application) => {
   const applicationApi = domainToApi.buildApplicationShape(application)
   const response = await apiService.submitApplication(applicationApi)
@@ -11,19 +9,4 @@ export const updateApplicationAction = async (application) => {
     Alerts.error()
   }
   return response
-
-  // const shortFormApplication = soqlToApi.buildApplicationShape(application)
-  // const updatedApplication = merge(shortFormApplication, {
-  //   primaryApplicant: {
-  //     maritalStatus: values.applicant.marital_status
-  //   },
-  //   numberOfDependents: values.number_of_dependents
-  // })
-  //
-  // let response = await apiService.submitApplication(updatedApplication)
-  // if (response === false) {
-  //   Alerts.error()
-  // }
-  //
-  // return response
 }
