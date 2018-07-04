d44162c9cb08ee1cbd2ae7430d58d41a594bad01
Federico <federico.dayan@gmail.com>
2018-07-04 12:01:10 -0300

    [#154747783] Preferences section fixed

--- spec/javascript/components/applications/ApplicationEditPage.test.js
+++ spec/javascript/components/applications/ApplicationEditPage.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import renderer from 'react-test-renderer';
+import { clone } from 'lodash'
 
 import ApplicationEditPage from 'components/applications/ApplicationEditPage'
-// import modelsFactory from '../../factories/models'
 import sharedHooks from '../../support/sharedHooks'
 
 import listing from '../../fixtures/listing'
@@ -12,6 +12,29 @@ describe('ApplicationNewPage', () => {
   sharedHooks.useFakeTimers()
 
   test('should render succesfully', () => {
+    const wrapper = renderer.create(
+      <ApplicationEditPage
+        listing={listing}
+        application={application}
+        editPage={true} />,
+    )
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  })
+
+  test('should render succesfully with preferences', () => {
+    const applicationWithPreferences = clone(application)
+
+    applicationWithPreferences.preferences[0].Application_Member = {
+      Date_of_Birth:"1981-05-04",
+      First_Name:"Flagby",
+      Id:"a0n0x000000B3xDAAS",
+      Last_Name:"Email"
+    }
+
+    expect(applicationWithPreferences.preferences).toHaveLength(1)
+    expect(applicationWithPreferences.preferences[0].Application_Member).toBeTruthy()
+
     const wrapper = renderer.create(
       <ApplicationEditPage
         listing={listing}
