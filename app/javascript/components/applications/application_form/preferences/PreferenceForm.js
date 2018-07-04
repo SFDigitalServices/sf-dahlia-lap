92a8f548fc2f78abdf97ef055093186730376833
Federico <federico.dayan@gmail.com>
2018-07-04 09:36:10 -0300

    [#154747783] Cleanup

--- app/javascript/components/applications/application_form/preferences/PreferenceForm.js
+++ app/javascript/components/applications/application_form/preferences/PreferenceForm.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import { Select } from 'react-form'
 import { find, map, omitBy } from 'lodash'
-// import CertOfPreferenceFields from './CertOfPreferenceFields'
-// import DisplacedFields from './DisplacedFields'
-// import AntiDisplacementFields from './AntiDisplacementFields'
-// import NeighborhoodResidentFields from './NeighborhoodResidentFields'
-// import LiveWorkFields from './LiveWorkFields'
-// import RentBurdenedAssistedHousingFields from './RentBurdenedAssistedHousingFields'
-// import DefaultPreferenceFields from './DefaultPreferenceFields'
-// import { naturalKeyFromMember } from './utils'
 import Row from '~/components/atoms/Row'
 import Column from '~/components/atoms/Column'
 import FormGroup from '~/components/atoms/FormGroup'
@@ -17,31 +9,6 @@ import PreferenceAdditionalOptions from './PreferenceAdditionalOptions'
 import { recordTypeMap } from './values'
 import { FIELD_NAME, buildFieldId } from './utils'
 
-// <<<<<<< HEAD
-// const PreferenceForm = ({ i, formApi, listingPreferences, fullHousehold }) => {
-//   const selectedPreference = () => {
-//     let selectedPreference = formApi.values.shortFormPreferences[i] || {}
-//     let matchedPreference = _.find(listingPreferences, (pref) => {
-//       return pref.id === selectedPreference.listingPreferenceID
-//     })
-//     if (!!matchedPreference && matchedPreference.lottery_preference) {
-//       let prefName = matchedPreference.lottery_preference.name
-//       formApi.values.shortFormPreferences[i].recordTypeDevName = recordTypeMap[prefName] || 'Custom'
-//     }
-//     return selectedPreference
-//   }
-//   const listingPreferencesOptions = _.map(_.omitBy(listingPreferences, (listingPref) => {
-//     // omit any listingPreferences that are already selected, excluding the current one
-//     let isSelected = _.find(formApi.values.shortFormPreferences, { listingPreferenceID: listingPref.id })
-//     return (isSelected && isSelected !== selectedPreference())
-//   }), (listingPref) => {
-//     // format them into form-friendly values
-//     return { value: listingPref.id, label: listingPref.lottery_preference.name }
-//   })
-//   const householdMembers = _.map(fullHousehold, (member) => {
-//     let natKey = naturalKeyFromMember(member)
-//     return { value: natKey, label: `${member.firstName} ${member.lastName}` }
-// =======
 const setRecordTypeDevName = (i, formApi, matched) => {
   if (!!matched && matched.Lottery_Preference) {
     let prefName = matched.Lottery_Preference.Name
