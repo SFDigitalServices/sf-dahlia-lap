2457d0270c85f9fe9906a9a1f5fc66f5c099781e
Federico <federico.dayan@gmail.com>
2018-07-13 18:14:59 -0300

    [#155196338] Rendering proof files

--- app/javascript/components/mappers/soqlToDomain/attachment.js
+++ app/javascript/components/mappers/soqlToDomain/attachment.js
@@ -1,6 +1,8 @@
 export const mapAttachment = (value) => {
   return {
     id: value.Id,
-    name: value.Name
+    name: value.Name,
+    related_application: value.Related_Application,
+    related_application_preference: value.Related_Application_Preference
   }
 }
