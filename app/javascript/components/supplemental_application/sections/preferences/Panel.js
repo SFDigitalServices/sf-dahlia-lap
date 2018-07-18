c195a1a490aaffc036b7970f988b318db182baf0
Federico <federico.dayan@gmail.com>
2018-07-18 15:13:04 -0300

    [#156708310] Adding Prefrence panel with Rent Burdened and Lives in SF WIP

--- app/javascript/components/supplemental_application/sections/preferences/Panel.js
+++ app/javascript/components/supplemental_application/sections/preferences/Panel.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Form, NestedForm, Text, Select } from 'react-form'
 
-const Panel = ({ onClose }) => {
+import FormGrid  from '~/components/molecules/FormGrid'
+import {
+  RentBurdenedPreference,
+  LiveOrWorkInSanFrancisco
+} from './preferences'
+
+const getPreferencePanel = (name) => {
+  if (name.match('Rent Burdened'))
+    return RentBurdenedPreference
+  else if (name.match('in San Francisco'))
+    return LiveOrWorkInSanFrancisco
+  else
+    return () => <div></div>
+}
+
+const Panel = ({ data, onClose }) => {
+  const PreferencePanel = getPreferencePanel(data[1])
   return (
     <div className="app-editable expand-wide scrollable-table-nested">
-      <div>Hello</div>
-      <br/>
-      <button type='button' className="button" onClick={(e) => (onClose())}>Close</button>
+      <PreferencePanel data={data}/>
+      <FormGrid.Row expand={false}>
+        <div className="form-grid_item column">
+      		<button className="button primary tiny margin-right margin-bottom-none" type="button" data-event="">Save</button>
+          <button className="button secondary tiny margin-bottom-none" type="button" data-event="" onClick={(e) => (onClose())}>Cancel</button>
+      	</div>
+      </FormGrid.Row>
     </div>
   )
 }
