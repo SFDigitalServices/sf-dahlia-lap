94b4bda4459af88adc75c449bc6df372a91dbd82
Federico <federico.dayan@gmail.com>
2018-07-19 15:13:28 -0300

    [#154064502] Lease up listing page header aligned to the right

--- app/javascript/components/lease_ups/LeaseUpListingsTable.js
+++ app/javascript/components/lease_ups/LeaseUpListingsTable.js
@@ -7,12 +7,12 @@ const PAGE_SIZE = 5
 const LeaseUpListingsTable = ({ listings, onCellClick }) => {
   const columns = [
     { Header: 'Listing Name', accessor: 'name', headerClassName: 'td-min-wide' },
-    { Header: 'Lottery Date', accessor: 'lottery_date', Cell: cellFormat.date },
-    { Header: 'Lottery Results Date', accessor: 'lottery_results_date', Cell: cellFormat.date },
-    { Header: 'Applications in Lottery', accessor: 'in_lottery' },
-    { Header: 'Total Units Available', accessor: 'units_available' },
-    { Header: 'Leases Signed', accessor: 'lease_signed_application' },
-    { Header: 'Last updated', accessor: 'last_modified_date', Cell: cellFormat.date }
+    { Header: 'Lottery Date', accessor: 'lottery_date', Cell: cellFormat.date, headerClassName: 'text-right', className: 'text-right' },
+    { Header: 'Lottery Results Date', accessor: 'lottery_results_date', Cell: cellFormat.date, minWidth: 120, headerClassName: 'text-right', className: 'text-right' },
+    { Header: 'Applications in Lottery', accessor: 'in_lottery', headerClassName: 'text-right', className: 'text-right' },
+    { Header: 'Total Units Available', accessor: 'units_available', headerClassName: 'text-right', className: 'text-right' },
+    { Header: 'Leases Signed', accessor: 'lease_signed_application', headerClassName: 'text-right', className: 'text-right' },
+    { Header: 'Last updated', accessor: 'last_modified_date', Cell: cellFormat.date, headerClassName: 'text-right', className: 'text-right' }
   ]
 
   const getTdProps = (state, rowInfo, column, instance) => {
