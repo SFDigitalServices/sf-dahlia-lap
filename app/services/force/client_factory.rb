59decef2570716926157b7b8774a9ae9ab62921b
Federico <federico.dayan@gmail.com>
2018-07-25 15:17:22 -0300

    [#156708310] Adding Custom Panel

--- app/services/force/client_factory.rb
+++ app/services/force/client_factory.rb
@@ -10,15 +10,19 @@ module Force
       end
     end
 
-    def new_with_username_and_password
-      Restforce.new(
+    def username_and_password_attributes
+      {
         username: ENV['SALESFORCE_USERNAME'],
         password: ENV['SALESFORCE_PASSWORD'],
         security_token: ENV['SALESFORCE_SECURITY_TOKEN'],
         client_id: ENV['SALESFORCE_CLIENT_ID'],
         client_secret: ENV['SALESFORCE_CLIENT_SECRET'],
         api_version: '41.0',
-      )
+      }
+    end
+
+    def new_with_username_and_password
+      Restforce.new(username_and_password_attributes)
     end
 
     def new_with_oauth_token(oauth_token)
