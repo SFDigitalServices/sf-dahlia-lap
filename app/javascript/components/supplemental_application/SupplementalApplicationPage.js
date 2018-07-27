bf1d85cd6d9875f2e0c1c159751efc6733268837
Federico <federico.dayan@gmail.com>
2018-07-27 09:58:51 -0300

    WIP

--- app/javascript/components/supplemental_application/SupplementalApplicationPage.js
+++ app/javascript/components/supplemental_application/SupplementalApplicationPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { findIndex, cloneDeep } from 'lodash'
 
 import appPaths from '~/utils/appPaths'
 import mapProps from '~/utils/mapProps'
@@ -8,42 +9,65 @@ import { mapList } from '~/components/mappers/utils'
 import CardLayout from '../layouts/CardLayout'
 import SupplementalApplicationContainer from './SupplementalApplicationContainer'
 
-const SupplementalApplicationPage = ({
-  application,
-  statusHistory,
-  formFields,
-  onSubmit,
-  fileBaseUrl,
-  onSavePreference,
-}) => {
-  const pageHeader = {
-    title: `${application.name}: ${application.applicant.name}`,
-    breadcrumbs: [
-      { title: 'Lease Ups', link: '/lease_ups' },
-      { title: application.listing.name, link: appPaths.toListingLeaseUps(application.listing.id) },
-      { title: application.name, link: '#' }
-    ]
+class SupplementalApplicationPage extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { application: props.application }
   }
 
-  const tabSection = {
-    items: [
-      { title: 'Short Form Application',    url: appPaths.toApplication(application.id) },
-      { title: 'Supplemental Information',  url: appPaths.toApplicationSupplementals(application.id) }
-    ]
+  handleSavePreference =  async(preference) => {
+    console.log('handleSavePreference')
+    const { onSavePreference } = this.props
+    const { application } = this.state
+
+    await onSavePreference(preference)
+
+    const prefIdx = findIndex(application.preferences, { id: preference.id })
+    const updatedApplication = cloneDeep(application)
+
+    updatedApplication.preferences[prefIdx] = preference
+    this.setState({ application: updatedApplication })
   }
 
-  return (
-    <CardLayout pageHeader={pageHeader} tabSection={tabSection}>
-      <SupplementalApplicationContainer
-        application={application}
-        statusHistory={statusHistory}
-        formFields={formFields}
-        onSubmit={onSubmit}
-        onSavePreference={onSavePreference}
-        fileBaseUrl={fileBaseUrl}
-      />
-    </CardLayout>
-  )
+  render() {
+    const {
+      statusHistory,
+      formFields,
+      onSubmit,
+      fileBaseUrl
+    } = this.props
+    const { application } = this.state
+
+    const pageHeader = {
+      title: `${application.name}: ${application.applicant.name}`,
+      breadcrumbs: [
+        { title: 'Lease Ups', link: '/lease_ups' },
+        { title: application.listing.name, link: appPaths.toListingLeaseUps(application.listing.id) },
+        { title: application.name, link: '#' }
+      ]
+    }
+
+    const tabSection = {
+      items: [
+        { title: 'Short Form Application',    url: appPaths.toApplication(application.id) },
+        { title: 'Supplemental Information',  url: appPaths.toApplicationSupplementals(application.id) }
+      ]
+    }
+
+    return (
+      <CardLayout pageHeader={pageHeader} tabSection={tabSection}>
+        <SupplementalApplicationContainer
+          application={application}
+          statusHistory={statusHistory}
+          formFields={formFields}
+          onSubmit={onSubmit}
+          onSavePreference={this.handleSavePreference}
+          fileBaseUrl={fileBaseUrl}
+        />
+      </CardLayout>
+    )
+  }
 }
 
 const mapProperties = ({application, statusHistory, file_base_url}) => {
