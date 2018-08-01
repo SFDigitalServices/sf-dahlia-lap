259f05771e775ae2af8837fe8cc956f4bf83045e
Federico <federico.dayan@gmail.com>
2018-08-01 13:15:22 -0300

    WIP

--- app/javascript/components/supplemental_application/actions.js
+++ app/javascript/components/supplemental_application/actions.js
@@ -9,8 +9,24 @@ export const updateApplicationAction = async (application) => {
     Alerts.error()
   }
   return response
+
+  // console.log('updateApplicationAction')
+  // return true
 }
 
-export const updateApplicationPreferenceAction = async (preference) => {
-  console.log(preference)
-}
+// export const updateApplicationPreferenceAction = async (preference) => {
+//   console.log('updateApplicationPreferenceAction')
+//   return true
+// }
+
+
+// applicationForApplicationPreference > save >
+// update general application with new saved preference
+
+// application global > save
+// update applicationForApplicationPreference with new application
+// ! it replaced current preferences array in applicationForApplicationPreference
+//
+
+
+// we save prefrences
