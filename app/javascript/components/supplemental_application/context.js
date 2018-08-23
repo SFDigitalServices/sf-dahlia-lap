10278dedbb265838e1af0307e82e837208db3a0a
Federico <federico.dayan@gmail.com>
2018-08-23 16:49:46 -0300

    [#159905145] Enzyme upgraded

--- app/javascript/components/supplemental_application/context.js
+++ app/javascript/components/supplemental_application/context.js
@@ -4,9 +4,11 @@ const Context = React.createContext();
 
 export default Context
 
-// This is being using for consuming context.
-// You have to wrap your component with this function to have access to the context.
-// The context is pass to your component as a property named `store`
+/*
+This is being using for consuming context.
+You have to wrap your component with this function to have access to the context.
+The context is pass to your component as a property named `store`
+*/
 export const withContext = (Component) => {
   const ContextComponent = (props) => {
     return (
