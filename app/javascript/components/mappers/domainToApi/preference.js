4e1809b8c388899fdec3666f6db764aa69cd0d4a
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-08-23 17:29:31 -0700

    [#158164230] Corrects mapping of some app pref fields

--- app/javascript/components/mappers/domainToApi/preference.js
+++ app/javascript/components/mappers/domainToApi/preference.js
@@ -1,12 +1,13 @@
+import { get } from 'lodash'
 import { createFieldMapper } from '~/utils/objectUtils'
 
 export const preferenceFieldMapper = {
   'recordtype_developername': 'recordTypeDevName',
   id: 'shortformPreferenceID',
-  'application_member__r.id': 'appMemberID',
+  appMemberID: (preference) => get(preference,'application_member.id'),
   certificate_number: 'certificateNumber',
   individual_preference: 'individualPreference',
-  lw_type_of_proof__c: 'lwPreferenceProof',
+  lw_type_of_proof: 'lwPreferenceProof',
   opt_out: 'optOut',
   type_of_proof: "preferenceProof",
   city: 'city',
