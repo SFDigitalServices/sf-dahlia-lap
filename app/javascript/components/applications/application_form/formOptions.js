6734c5e69c1c10f6836ab6be08ee79ec811d79a9
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-08-29 14:37:24 -0400

    [#158164230] Adds pref proof types for Work in SF

--- app/javascript/components/applications/application_form/formOptions.js
+++ app/javascript/components/applications/application_form/formOptions.js
@@ -114,6 +114,11 @@ const preference_proof_options_nrhp = labelize([
   'Letter documenting homelessness',
 ])
 
+const preference_proof_options_work_in_sf = labelize([
+  'Paystub with employer address',
+  'Letter from employer',
+])
+
 const priority_options = [
   'Mobility impaired',
   'Vision impaired',
@@ -157,6 +162,7 @@ export default {
   preference_proof_options_default,
   preference_proof_options_rent_burden,
   preference_proof_options_nrhp,
+  preference_proof_options_work_in_sf,
   priority_options,
   listing_referral_options,
   yes_no_options,
