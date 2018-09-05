73efc929478a805671a67bb4bd1cf70c05f30ade
Federico <federico.dayan@gmail.com>
2018-09-05 16:32:46 -0300

    [#155196495] Adding docs

--- app/services/force/units_service.rb
+++ app/services/force/units_service.rb
@@ -1,10 +1,12 @@
 module Force
-  # encapsulate all Salesforce Short Form Application querying functions
+  # Encapsulate all Salesforce Short Form Application querying functions
   class UnitsService < Force::Base
     DRAFT = 'Draft'.freeze
     FIELD_NAME = :units
     FIELDS = load_fields(FIELD_NAME).freeze
 
+    # Returns the available units (units not assigned to any application) for
+    # the specified listing, including the unit for the specified application.
     def available_units_for_application(listing_id, application_id)
       sub_query = builder.from(:Lease__c)
                          .select(:Unit__c)
@@ -21,6 +23,8 @@ module Force
              .records
     end
 
+    # Returns all the units for the specified listing that
+    # do not have an application assigned yet
     def unassigned_units_by_listing(listing_id)
       sub_query = builder.from(:Lease__c)
                          .select(:Unit__c)
@@ -36,6 +40,8 @@ module Force
              .records
     end
 
+    # Returns all the units for the specified listing that
+    # have an application assigned
     def assigned_units_by_listing(listing_id)
       builder.from(:Lease__c)
              .select('Id, Unit__c')
