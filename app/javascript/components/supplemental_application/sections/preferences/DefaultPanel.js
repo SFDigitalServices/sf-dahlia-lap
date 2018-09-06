e6105c9bdcc6acda92c168a58a483c50b9e51738
Federico <federico.dayan@gmail.com>
2018-09-06 14:45:05 -0300

    [#160325788] Updated camel case fields from domain to extract from snake case

--- app/javascript/components/supplemental_application/sections/preferences/DefaultPanel.js
+++ app/javascript/components/supplemental_application/sections/preferences/DefaultPanel.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const DefaultPanel = () => {
-  return (<div />)
+  return (<div> This is the default panel. Something might be wrong</div>)
 }
 
 export default DefaultPanel
