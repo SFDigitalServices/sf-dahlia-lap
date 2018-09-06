42cafc3aec8c43065919af7db95e642392d81e01
Federico <federico.dayan@gmail.com>
2018-09-06 15:52:19 -0300

    [160325788] Checking for applicant member. Rolling back message

--- app/javascript/components/supplemental_application/sections/preferences/DefaultPanel.js
+++ app/javascript/components/supplemental_application/sections/preferences/DefaultPanel.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const DefaultPanel = () => {
-  return (<div> This is the default panel. Something might be wrong</div>)
+  return (<div />)
 }
 
 export default DefaultPanel
