5f236b579c28e8b7f21076c1d0155ceb9674ca0c
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-09-27 11:01:41 -0700

    [#155196224] Updates supp app status change e2e test

--- spec/javascript/support/puppeteer/steps/supplementalApplicationSteps.js
+++ spec/javascript/support/puppeteer/steps/supplementalApplicationSteps.js
@@ -1,5 +1,3 @@
-import utils from '../utils'
-
 const testStatusModalUpdate = async (page) => {
   const COMMENT = 'This is a comment.'
 
@@ -17,13 +15,8 @@ const testStatusModalUpdate = async (page) => {
   // Submit the status modal form
   await page.click('.form-modal_form_wrapper button.primary')
 
-  // The page should enter a loading state while the status modal
-  // submit action is in progress
-  const pageHasLoadingState = await utils.isPresent(page, '.tabs-card-row > .tabs-card > .loading-panel.loading')
-  expect(pageHasLoadingState).toBe(true)
-
   // Wait for the page to reload after the status modal submit
-  await page.waitForSelector('.tabs-card-row > .tabs-card > .loading-panel:not(.loading)')
+  await page.waitForNavigation()
 
   // The latest status in the status history should be the status
   // that was just selected and saved
