5c995d2065f56fb887b28525075b49cc18a404e2
Federico <federico.dayan@gmail.com>
2018-10-01 15:38:33 -0300

    [#159935677] Lint fixed

--- app/javascript/components/supplemental_application/sections/preferences/AntiDisplacementHousingPanel.js
+++ app/javascript/components/supplemental_application/sections/preferences/AntiDisplacementHousingPanel.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Select, Text } from 'react-form'
+import { Select } from 'react-form'
 
 import FormGrid from '~/components/molecules/FormGrid'
 import { FormItem, Comment, SelectStatus } from './utils'
