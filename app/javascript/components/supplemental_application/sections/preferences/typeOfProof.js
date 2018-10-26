444174fd8623ddcce0fd17ba41a1b67587259723
Christine Dolendo <chris.dolendo@exygy.com>
2018-10-26 09:04:02 -0700

    [#160564960] - Simplify logic and refactor code

--- app/javascript/components/supplemental_application/sections/preferences/typeOfProof.js
+++ app/javascript/components/supplemental_application/sections/preferences/typeOfProof.js
@@ -38,17 +38,15 @@ const TypeOfProofWithoutFile = ({type}) => {
 }
 
 const getAttachments = (preference, proofFiles, fileBaseUrl) => {
-  let typeOfProof = preference.recordtype_developername === 'L_W' ? preference.lw_type_of_proof : preference.type_of_proof
   let proofFilesForPreference = filter(proofFiles, { related_application_preference: preference.id })
+  let typeOfProof = preference.recordtype_developername === 'L_W' ? preference.lw_type_of_proof : preference.type_of_proof
+  let typeOfProofMatchesFileType = some(proofFilesForPreference) && typeOfProof === proofFilesForPreference[0].document_type
+  let isRentBurdened = preference.recordtype_developername === 'RB_AHP'
 
-  let isRentBurdenedAWithFiles = preference.recordtype_developername === 'RB_AHP' && some(proofFilesForPreference)
-  let isNotRentBurdenedWithFiles = preference.recordtype_developername !== 'RB_AHP' && some(proofFilesForPreference)
-  let typeOfProofMatchesFileType = typeOfProof === proofFilesForPreference[0].document_type
-
-  if (isRentBurdenedAWithFiles || (isNotRentBurdenedWithFiles && typeOfProofMatchesFileType)) {
-    return <ProofFilesList proofFiles={proofFilesForPreference} fileBaseUrl={fileBaseUrl} />
-  } else if (isNotRentBurdenedWithFiles && !typeOfProofMatchesFileType) {
+  if (typeOfProof && !typeOfProofMatchesFileType && !isRentBurdened) {
     return <TypeOfProofWithoutFile type={typeOfProof} />
+  } else {
+    return <ProofFilesList proofFiles={proofFilesForPreference} fileBaseUrl={fileBaseUrl} />
   }
 }
 
