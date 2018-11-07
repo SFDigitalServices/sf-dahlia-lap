1ab5e2f90fb83409d65b7014344ed77d0a641b83
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-11-07 10:57:46 -0800

    [#158503202] WIP - further develops the rental assistance update e2e test

--- spec/javascript/e2e/SupplementalApplicationPage.e2e.js
+++ spec/javascript/e2e/SupplementalApplicationPage.e2e.js
@@ -56,18 +56,19 @@ describe('SupplementalApplicationPage', () => {
     await page.waitForSelector('.rental-assistance-new-form')
 
     // Set up the values we'll use to fill out the rental assistance form
-    const [typeVal] = await page.$eval(
+    const [typeVal, typeLabel] = await page.$eval(
       '.rental-assistance-new-form .rental-assistance-type option:nth-child(2)',
       e => [e.value, e.textContent]
     )
+    const recurring = 'Yes'
     const amount = 100
-    const [recipientVal] = await page.$eval(
+    const [recipientVal, recipientLabel] = await page.$eval(
       '.rental-assistance-new-form .rental-assistance-recipient option:nth-child(2)',
       e => [e.value, e.textContent]
     )
 
     // Record how many rental assistances are currently in the table
-    const tableSize = await page.$$eval('.rental-assistances > tbody > .tr-expand', e => e.length)
+    const prevTableSize = await page.$$eval('.rental-assistances > tbody > .tr-expand', elems => elems.length)
 
     // Fill out the new rental assistance form
     await page.select('.rental-assistance-new-form .rental-assistance-type', typeVal)
@@ -82,8 +83,19 @@ describe('SupplementalApplicationPage', () => {
     await page.waitForResponse(request => request.url().includes('http://localhost:3000/api/v1/rental-assistances'))
 
     // Check that the rental assistances table has increased in size by one
-    const currentTableSize = await page.$$eval('.rental-assistances > tbody > .tr-expand', e => e.length)
-    expect(currentTableSize).toEqual(tableSize + 1)
+    const newTableSize = await page.$$eval('.rental-assistances > tbody > .tr-expand', elems => elems.length)
+    expect(newTableSize).toEqual(prevTableSize + 1)
+
+    // Check that the last rental assistance in table has the values we saved
+    const lastRentalAssistanceSelector = '.rental-assistances > tbody > .tr-expand:nth-last-child(2)'
+    const newTypeLabel = await page.$eval(`${lastRentalAssistanceSelector} td:nth-child(1)`, e => e.textContent)
+    const newRecurring = await page.$eval(`${lastRentalAssistanceSelector} td:nth-child(2)`, e => e.textContent)
+    const newAmount = await page.$eval(`${lastRentalAssistanceSelector} td:nth-child(3)`, e => e.textContent)
+    const newRecipient = await page.$eval(`${lastRentalAssistanceSelector} td:nth-child(4)`, e => e.textContent)
+    expect(newTypeLabel).toEqual(typeLabel)
+    expect(newRecurring).toEqual(recurring)
+    expect(newAmount).toEqual(`$${amount}`)
+    expect(newRecipient).toEqual(recipientLabel)
 
     await browser.close()
   }, DEFAULT_E2E_TIME_OUT)
@@ -91,26 +103,36 @@ describe('SupplementalApplicationPage', () => {
   // This test requires a rental assistance to be already present in the rental
   // assistances table. If the prior test for creating a rental assistance has
   // succeeded, then there will be at least one rental assistance present.
-  test('should allow a rental assistance to be updated', async () => {
-    let browser = await puppeteer.launch({ headless: HEADLESS })
+  test.only('should allow a rental assistance to be updated', async () => {
+    let browser = await puppeteer.launch({ headless: false, slowMo: 50 })
     let page = await browser.newPage()
 
     await sharedSteps.loginAsAgent(page)
     await sharedSteps.goto(page, `/applications/${LEASE_UP_LISTING_APPLICATION_ID}/supplementals`)
 
-    // Locate the first rental assistance
-
-    // Click the Edit button on that rental assistance
+    // Click the Edit button on the first rental assistance
+    const firstRentalAssistanceSelector = '.rental-assistances > tbody > .tr-expand:first-child'
+    await page.click(`${firstRentalAssistanceSelector} button.action-link`)
 
     // Wait for the edit rental assistance form to open
+    await page.waitForSelector('.rental-assistance-edit-form')
+
+    // Record the amount value
+    const prevAmountValue = await page.$eval('.rental-assistance-edit-form #assistance_amount', e => e.value)
 
     // Change the amount field
+    const amount = parseInt(prevAmountValue) + 100
+    await page.type('.rental-assistance-edit-form #assistance_amount', `${amount}`)
 
     // Save the edit rental assistance form
+    await page.click('.rental-assistance-edit-form button.primary')
 
     // Wait for the API rental assistance update call to complete
+    await page.waitForResponse(request => request.url().includes('http://localhost:3000/api/v1/rental-assistances'))
 
     // Check that the first rental assistance's amount value matches the value we updated it to
+    const newAmount = await page.$eval(`${firstRentalAssistanceSelector} td:nth-child(3)`, e => e.textContent)
+    expect(newAmount).toEqual(`$${amount}`)
 
     await browser.close()
   }, DEFAULT_E2E_TIME_OUT)
