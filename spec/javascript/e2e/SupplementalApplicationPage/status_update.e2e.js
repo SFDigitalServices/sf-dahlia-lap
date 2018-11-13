5c86c513bed4705deb711f9149c04febfab091bd
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-11-13 11:55:10 -0800

    [#158503202] Renames supp app e2e tests

--- spec/javascript/e2e/SupplementalApplicationPage/status_update.e2e.js
+++ spec/javascript/e2e/SupplementalApplicationPage/status_update.e2e.js
@@ -8,6 +8,23 @@ import {
   HEADLESS
 } from '../../support/puppeteer/consts'
 
+describe('SupplementalApplicationPage status history', () => {
+  test('should allow status updates via the Add a Comment button in the status history section', async () => {
+    let browser = await puppeteer.launch({ headless: HEADLESS })
+    let page = await browser.newPage()
+
+    await sharedSteps.loginAsAgent(page)
+    await sharedSteps.goto(page, `/applications/${LEASE_UP_LISTING_APPLICATION_ID}/supplementals`)
+
+    // Click on the "Add a Comment" button in the Status History section
+    await page.click('.status-list_footer button')
+
+    await supplementalApplicationSteps.testStatusModalUpdate(page)
+
+    await browser.close()
+  }, DEFAULT_E2E_TIME_OUT)
+})
+
 describe('SupplementalApplicationPage action buttons', () => {
   test('should allow status updates via the status dropdown at the bottom of the page', async () => {
     let browser = await puppeteer.launch({ headless: HEADLESS })
