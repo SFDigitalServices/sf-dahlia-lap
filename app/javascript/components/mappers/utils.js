1e8beffee5802dee40ae25c8250820199ead34c3
Christine Dolendo <chris.dolendo@exygy.com>
2018-11-26 15:18:59 -0800

    [#161459497] - Allow for blank objects and arrays in mapping for empty values

--- app/javascript/components/mappers/utils.js
+++ app/javascript/components/mappers/utils.js
@@ -1,11 +1,11 @@
 import { omitBy, isUndefined } from 'lodash'
 
 export const mapShape = (mapper, value) => {
-  if (value) { return mapper(value) } else { return undefined }
+  if (value) { return mapper(value) } else { return {} }
 }
 
 export const mapList = (mapper, list) => {
-  if (list) { return list.map(mapper) } else { return undefined }
+  if (list) { return list.map(mapper) } else { return [] }
 }
 
 export const compactShape = obj => omitBy(obj, isUndefined)
