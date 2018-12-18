31f4fe0d90a475618f02966ffec2152f72dcbc06
Andrea Egan <aegan526@gmail.com>
2018-12-18 11:30:10 -0800

    [#162422018] Permit lwPreferenceProof in shortform controller

--- app/controllers/api/v1/short_form_controller.rb
+++ app/controllers/api/v1/short_form_controller.rb
@@ -144,6 +144,7 @@ class Api::V1::ShortFormController < ApiController
               certificateNumber
               naturalKey
               preferenceProof
+              lwPreferenceProof
               preferenceName
               individualPreference
               optOut
