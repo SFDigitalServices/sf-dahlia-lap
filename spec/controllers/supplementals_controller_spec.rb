5887f189e3ecfca644a1a27d79ef9009e47a055d
Andrea Egan <aegan526@gmail.com>
2019-01-17 16:46:37 -0800

    [#162422625] Update rspec vcr tests
    Many VCRs needed to be updated post-refresh

--- spec/controllers/supplementals_controller_spec.rb
+++ spec/controllers/supplementals_controller_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe Applications::SupplementalsController, type: :controller do
       VCR.use_cassette('supplementals_controller/index/supplemental_application') do
         get :index, params: { application_id: lease_up_application_id }
         application = assigns(:application)
+        rental_assistances = assigns(:rental_assistances)
+        units = assigns(:units)
+        available_units = assigns(:available_units)
+
+        expect(rental_assistances).not_to be_empty
+        expect(units).not_to be_empty
+        expect(available_units).not_to be_empty
+
+        expect(application['preferences']).not_to be_empty
+        expect(application['proof_files']).not_to be_empty
+        expect(application['lease']).not_to be_empty
+        expect(application['household_members'].length).to eq(0)
         expect(application).to eq(expected_supplemental_lease_up_app)
       end
     end
