306dffe8c8ef579c5bcfcea31baff021456f793f
Christine Dolendo <chris.dolendo@exygy.com>
2019-03-11 14:12:46 -0700

    test commit

--- app/services/force/rest/lease_service.rb
+++ app/services/force/rest/lease_service.rb
@@ -9,6 +9,11 @@ module Force
         @client.create!('Lease__c', lease.to_salesforce)
       end
 
+      def dummy_create(domain_attrs)
+        lease = Force::Lease.from_domain(domain_attrs)
+        @client.create!('Lease__c', lease.to_salesforce)
+      end
+
       def update(domain_attrs)
         lease = Force::Lease.from_domain(domain_attrs)
         @client.update!('Lease__c', lease.to_salesforce)
