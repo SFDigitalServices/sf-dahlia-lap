2676f0e94e295f0211defe4df3e89512f93a6a88
Andrea Egan <aegan526@gmail.com>
2019-03-13 16:12:46 -0700

    [#164619509] Add no preference used, id to lease mapper

--- app/javascript/components/mappers/domainToApi/lease.js
+++ app/javascript/components/mappers/domainToApi/lease.js
@@ -3,9 +3,11 @@ import { currencyToFloat } from '~/utils/utils'
 
 // Lease is sent to backend with domain keys.
 export const leaseFieldMapper = {
+  'id': 'id',
   'lease_start_date': 'lease_start_date',
-  'unit': 'unit',
+  'no_preference_used': 'no_preference_used',
   'preference_used': 'preference_used',
+  'unit': 'unit',
   total_monthly_rent_without_parking: (source) => currencyToFloat(source.total_monthly_rent_without_parking),
   monthly_parking_rent: (source) => currencyToFloat(source.monthly_parking_rent),
   monthly_tenant_contribution: (source) => currencyToFloat(source.monthly_tenant_contribution)
