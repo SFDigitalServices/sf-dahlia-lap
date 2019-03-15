be5b8ae40f5f6b76c6cda713e4a6b48b187e33e9
Andrea Egan <aegan526@gmail.com>
2019-03-15 15:11:47 -0700

    [#164589943] Add updateApplication tests for success cases

--- spec/javascript/components/supplemental_application/actions.test.js
+++ spec/javascript/components/supplemental_application/actions.test.js
@@ -3,24 +3,64 @@ import supplementalApplication from '../../fixtures/supplemental_application'
 import { mapApplication } from '~/components/mappers/soqlToDomain'
 import mockSupplementalSubmitPayload from '../../fixtures/supplemental_submit_payload'
 
-const mockFn = jest.fn()
+const mockSubmitAppFn = jest.fn()
+const mockSubmitLeaseFn = jest.fn()
 
 jest.mock('apiService', () => {
   const mockSubmitApplication = async (data) => {
-    mockFn(data)
+    mockSubmitAppFn(data)
     return true
   }
 
-  return { submitApplication: mockSubmitApplication }
+  const mockCreateOrUpdateLease = async (data) => {
+    mockSubmitLeaseFn(data)
+    return true
+  }
+
+  return {
+    submitApplication: mockSubmitApplication,
+    createOrUpdateLease: mockCreateOrUpdateLease
+  }
 })
 
-describe('actions', () => {
-  test('it should submit to shortForm', async () => {
+describe('updateApplication', () => {
+  test('it should submit application to shortForm endpoint', async () => {
     const applicationDomain = mapApplication(supplementalApplication)
     const response = await updateApplication(applicationDomain)
 
     expect(response).toEqual(true)
-    expect(mockFn.mock.calls.length).toEqual(1)
-    expect(mockFn.mock.calls[0][0]).toEqual(mockSupplementalSubmitPayload)
+    expect(mockSubmitAppFn.mock.calls.length).toEqual(1)
+    expect(mockSubmitAppFn.mock.calls[0][0]).toEqual(mockSupplementalSubmitPayload)
+  })
+  test('it should submit lease to backend if lease is present in application', async () => {
+    const application = {
+      'id': 'appID',
+      'name': 'APP-12345',
+      'listing': {
+        'id': 'listingID'
+      },
+      'lease': {
+        'id': 'leaseID'
+      }
+    }
+
+    const response = await updateApplication(application)
+    expect(response).toEqual(true)
+    expect(mockSubmitAppFn.mock.calls.length).toEqual(1)
+    expect(mockSubmitLeaseFn.mock.calls.length).toEqual(1)
+  })
+  test('it should not submit lease to backend if application has no lease', async () => {
+    const application = {
+      'id': 'appID',
+      'name': 'APP-12345',
+      'listing': {
+        'id': 'listingID'
+      },
+      'lease': {}
+    }
+    const response = await updateApplication(application)
+    expect(response).toEqual(true)
+    expect(mockSubmitAppFn.mock.calls.length).toEqual(1)
+    expect(mockSubmitLeaseFn.mock.calls.length).toEqual(0)
   })
 })
