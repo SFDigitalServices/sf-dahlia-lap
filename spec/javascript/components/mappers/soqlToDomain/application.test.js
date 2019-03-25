12ba5d1150142c5e3c8b80573af8e883e2430dd2
Andrea Egan <aegan526@gmail.com>
2019-03-25 14:53:17 -0700

    [#162933550] Simplify mapApplication test
    Only test demographics info

--- spec/javascript/components/mappers/soqlToDomain/application.test.js
+++ spec/javascript/components/mappers/soqlToDomain/application.test.js
@@ -1,11 +1,26 @@
-// TODO: Expand these tests to use fixtures, test all fields in application.
+// TODO: Expand these tests to test all fields in application.
 import { mapApplication } from '~/components/mappers/soqlToDomain'
 import soqlApplication from '../../../fixtures/application'
-import domainApplication from '../../../fixtures/domain_application'
 
 describe('mapApplication', () => {
-  test('it should build map soql application to domain', async () => {
+  test('it should create dempgraphics based on primary applicant values', async () => {
+    soqlApplication.Ethnicity = 'Decline to state'
+    soqlApplication.Race = 'Decline to state'
+    soqlApplication.Gender = 'Decline to state'
+    soqlApplication.Gender_Other = 'gender other'
+    soqlApplication.Sexual_Orientation = 'Decline to state'
+    soqlApplication.Sexual_Orientation_Other = 'sexual orientation other'
+
     const mappedApplication = mapApplication(soqlApplication)
-    expect(mappedApplication).toEqual(domainApplication)
+
+    const expectedDomainDemographics = {
+      'ethnicity': 'Decline to state',
+      'gender': 'Decline to state',
+      'gender_other': 'gender other',
+      'race': 'Decline to state',
+      'sexual_orientation': 'Decline to state',
+      'sexual_orientation_other': 'sexual orientation other'
+    }
+    expect(mappedApplication['demographics']).toEqual(expectedDomainDemographics)
   })
 })
