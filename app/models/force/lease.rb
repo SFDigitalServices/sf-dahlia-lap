ca7966a71a5f691a986661c84e8c7e99c74e4fab
Andrea Egan <aegan526@gmail.com>
2019-04-05 14:10:27 -0700

    [#163398177] Add rspec tests for date conversion

--- app/models/force/lease.rb
+++ app/models/force/lease.rb
@@ -21,8 +21,18 @@ module Force
 
     def to_salesforce
       salesforce_fields = super
+
       # Convert array date into string
-      salesforce_fields['Lease_Start_Date'] = date_to_salesforce(@fields.domain['lease_start_date'])
+      if @fields.domain['lease_start_date']
+        salesforce_fields['Lease_Start_Date'] = self.class.date_to_salesforce(@fields.domain['lease_start_date'])
+      end
+
+      # Special field conversion case for preference used
+      if @fields.domain.present?
+        # Salesforce requires true/false for No_Preference_Used__c even if field was untouched
+        no_pref_used_value = @fields.domain['no_preference_used']
+        salesforce_fields['No_Preference_Used'] = no_pref_used_value.nil? ? false : no_pref_used_value
+      end
 
       # Add the "__c" suffix back onto Salesforce field names
       field_names = salesforce_fields.keys
@@ -33,30 +43,26 @@ module Force
         end
       end
 
-      # Special field conversion cases for leases
-      if @fields.domain.present?
-        # Salesforce requires true/false for No_Preference_Used__c even if field was untouched
-        no_pref_used_value = @fields.domain['no_preference_used']
-        salesforce_fields['No_Preference_Used__c'] = no_pref_used_value.nil? ? false : no_pref_used_value
-      end
-
       salesforce_fields
     end
 
     def to_domain
       domain_fields = super
-      @fields.domain['lease_start_date']
-      domain_fields.lease_start_date = date_to_domain(@fields.domain['lease_start_date'])
+
+      if @fields.domain['lease_start_date']
+        domain_fields['lease_start_date'] = self.class.date_to_domain(@fields.domain['lease_start_date'])
+      end
+
       domain_fields
     end
 
-    def date_to_domain(api_date)
+    def self.date_to_domain(api_date)
       # Convert string date into array
-      return nil unless api_date
+      return nil if api_date.blank?
       api_date.split('-')
     end
 
-    def date_to_salesforce(domain_date)
+    def self.date_to_salesforce(domain_date)
       return nil unless domain_date&.any?(&:present?)
       lease_date = Date.new(domain_date[0].to_i, domain_date[1].to_i, domain_date[2].to_i)
       lease_date.strftime('%F')
