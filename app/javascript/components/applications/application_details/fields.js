f69ce7a24917ca29d401bfc140b814d063b4e688
Marcin Jedras <mjjedras@gmail.com>
2019-04-10 18:11:20 +0200

    [#162591164] Add missing fields to application view

--- app/javascript/components/applications/application_details/fields.js
+++ app/javascript/components/applications/application_details/fields.js
@@ -16,8 +16,10 @@ export const applicationDataFields = [
 export const primaryApplicantFields = [
   'first_name',
   'last_name',
+  'middle_name',
   'date_of_birth',
   'phone',
+  'phone_type',
   'second_phone',
   'email',
   'residence_address',
@@ -27,7 +29,9 @@ export const primaryApplicantFields = [
 export const alternateContactFields = [
   'first_name',
   'last_name',
+  'middle_name',
   'phone',
+  'phone_type',
   'agency_name',
   'email',
   'alternate_contact_type',
