b8b1fb99a5851eadfc8707b8e61115ec4dfeb5d0
Andrea Egan <aegan526@gmail.com>
2019-04-12 15:53:48 -0700

    [#165160753] Use env var for DSN variables

--- app/javascript/packs/sentry.js
+++ app/javascript/packs/sentry.js
@@ -1,5 +1,6 @@
+/* global SENTRY_PUBLIC_DSN */
 import * as Sentry from '@sentry/browser'
 
 Sentry.init({
-  dsn: 'https://220b63fca88041809b887a91c5f3701d:a4d342cd73de4e9f9fb343d7edd87d92@sentry.io/1438051'
+  dsn: SENTRY_PUBLIC_DSN
 })
--- config/sentry.rb
+++ config/sentry.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Raven.configure do |config|
-  config.dsn = 'https://220b63fca88041809b887a91c5f3701d:a4d342cd73de4e9f9fb343d7edd87d92@sentry.io/1438051'
+  config.dsn = ENV['SENTRY_PRIVATE_DSN']
 end
