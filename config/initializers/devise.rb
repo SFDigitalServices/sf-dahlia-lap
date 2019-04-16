a916ea25c0289e5608c22284d0bea4437df8aae2
Christine Dolendo <chris.dolendo@exygy.com>
2019-04-16 06:46:28 -0700

    [#162316506] - For testing, change to timeout in 30 minutes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -167,7 +167,8 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 30.minutes
+  # TO DO BEFORE MERGE: CHANGE TO 10.seconds
+  config.timeout_in = 5.seconds
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
