7432a5b4e7648a44df4b03e16a55b768fbb40e07
Andrea Egan <aegan526@gmail.com>
2019-04-18 10:45:12 -0700

    [#165160753] Update sentry webpack plugin ignored directories

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 const dotenv = require('dotenv')
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
 const dotenvFiles = [
   '.env'
@@ -10,5 +11,11 @@ dotenvFiles.forEach((dotenvFile) => {
 })
 
 environment.plugins.prepend('Environment', new webpack.DefinePlugin({'process.env': {'SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN)}}))
+environment.plugins.prepend('SentryWebpack', new SentryWebpackPlugin({
+  include: '.',
+  ignoreFile: '.sentrycliignore',
+  ignore: ['node_modules', 'webpack.config.js', 'spec', 'config', 'coverage'],
+  configFile: 'sentry.properties'
+}))
 
 module.exports = environment
