a0d81aece3b665a836025d6dff70c9fda6cdb692
Christine Dolendo <chris.dolendo@exygy.com>
2019-04-25 15:06:01 -0700

    [#162316506] - Update session expiration to 30 mins

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -168,7 +168,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 15.minutes
+  config.timeout_in = 30.minutes
 
   # Adds a failure app that logs out the users from salesforce
   config.warden do |manager|
