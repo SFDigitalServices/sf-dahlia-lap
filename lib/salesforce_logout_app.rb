6dbe78608490f7c2084939fdde4dd1e765e0e7df
Christine Dolendo <chris.dolendo@exygy.com>
2019-04-26 14:42:57 -0700

    Fix error on revoking expired tokens #162316506  (#219)
    * [#162316506] - Fix error on revoking expired tokens
    
    * [#162316506] - Write test for logging in and out successfully
    
    * [#162316506] - Implement timeout for user sessions
    
    * [#162316506] - For testing, change to timeout in 30 minutes
    
    * "[#162316506] - WIP Logout user out of salesforce when their session has timed out"
    
    * [#162316506] Redirect to salesforce logout on timeout
    
    * [#162316506] - Cleanup code
    
    * [#162316506] - Update session expiration to 30 mins

---
+++ lib/salesforce_logout_app.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Handles Salesforce logouts when user session is timed out.
+class SalesforceLogoutApp < Devise::FailureApp
+  def redirect
+    store_location!
+    message = warden.message || warden_options[:message]
+
+    # For some reason it also needs to be called when message is nil
+    if message.nil? || message == :timeout
+      @salesforce_logout_host = warden.params[:admin] ? ENV['SALESFORCE_INSTANCE_URL'] : ENV['COMMUNITY_LOGIN_URL']
+      redirect_to "#{@salesforce_logout_host}/secur/logout.jsp" and return
+    else
+      super
+    end
+  end
+end
