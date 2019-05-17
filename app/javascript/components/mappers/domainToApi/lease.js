8b7aedcc689405fd02ded1aa4b6b496ccddf6ee2
Marcin Jedras <mjjedras@gmail.com>
2019-05-17 17:31:54 +0200

    [#165735950] Upgrade leasing information to final form

--- app/javascript/components/mappers/domainToApi/lease.js
+++ app/javascript/components/mappers/domainToApi/lease.js
@@ -1,10 +1,11 @@
 import { createFieldMapper } from '~/utils/objectUtils'
 import { currencyToFloat } from '~/utils/utils'
+import { isEmpty } from 'lodash'
 
 // Lease is sent to backend with domain keys.
 export const leaseFieldMapper = {
   'id': 'id',
-  'lease_start_date': 'lease_start_date',
+  'lease_start_date': (source) => !isEmpty(source.lease_start_date) ? [source.lease_start_date.year, source.lease_start_date.month, source.lease_start_date.day] : '',
   'no_preference_used': 'no_preference_used',
   'preference_used': 'preference_used',
   'unit': 'unit',
--- app/javascript/utils/form/final_form/MultiDateField.js
+++ app/javascript/utils/form/final_form/MultiDateField.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { BlockNote } from '~/utils/form/Field'
 import { maxLengthMap } from '~/utils/formUtils'
 import { InputField } from '~/utils/form/final_form/Field'
-import { every } from 'lodash'
+import { every, last } from 'lodash'
+
 import classNames from 'classnames'
 
 export const MultiDateField = ({ form, fieldName, formName, index, label, blockNote, id }) => {
@@ -15,8 +16,8 @@ export const MultiDateField = ({ form, fieldName, formName, index, label, blockN
   } else {
     errorField = form.getState().errors[fieldName.split('.')[0]]
   }
-
-  const error = form && touched && errorField && errorField['date_of_birth'] && errorField['date_of_birth']['all']
+  const baseFieldName = last(fieldName.split('.'))
+  const error = form && touched && errorField && errorField[baseFieldName] && errorField[baseFieldName]['all']
   return (
     <div className={classNames('form-group', (error && 'error') || '')}>
       <label className='form-label' htmlFor={id || `form-${fieldName}`}>
