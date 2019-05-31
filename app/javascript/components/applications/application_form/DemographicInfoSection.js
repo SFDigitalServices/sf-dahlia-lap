0f4f40ce5acd4d4790d3118267a7bbe983eb8aef
Logan Arnett <logan@loganarnett.com>
2019-05-30 22:06:06 -0400

    cognitive complexity update

--- app/javascript/components/applications/application_form/DemographicInfoSection.js
+++ app/javascript/components/applications/application_form/DemographicInfoSection.js
@@ -11,55 +11,57 @@ let {
   ethnicityOptions
 } = formOptions
 
-const DemographicInfoSection = ({ values: { demographics } }) => (
-  <div className='border-bottom margin-bottom--2x'>
-    <h3>Demographic Information</h3>
-    <div className='row'>
-      <div className='small-6 columns'>
-        <SelectField
-          fieldName='demographics.ethnicity'
-          label='Ethnicity'
-          options={ethnicityOptions}
-          validation={validate.isPresent('Ethnicity is required')} />
+const DemographicInfoSection = ({ values: { demographics } }) => {
+  return (
+    <div className='border-bottom margin-bottom--2x'>
+      <h3>Demographic Information</h3>
+      <div className='row'>
+        <div className='small-6 columns'>
+          <SelectField
+            fieldName='demographics.ethnicity'
+            label='Ethnicity'
+            options={ethnicityOptions}
+            validation={validate.isPresent('Ethnicity is required')} />
+        </div>
+        <div className='small-6 columns'>
+          <SelectField
+            fieldName='demographics.race'
+            label='Race'
+            options={raceOptions}
+            validation={validate.isPresent('Race is required')} />
+        </div>
+        <div className='small-6 columns'>
+          <SelectField
+            fieldName='demographics.gender'
+            label='Gender'
+            options={genderOptions}
+            validation={!demographics || (demographics && !demographics.gender_other) ? validate.isPresent('Gender is required') : ''} />
+        </div>
+        <div className='small-6 columns'>
+          <InputField
+            fieldName='demographics.gender_other'
+            label='Gender Specify (if not listed)'
+            maxLength={maxLengthMap['gender_other']}
+            validation={!demographics || (demographics && !demographics.gender) ? validate.isPresent('Gender is required') : ''} />
+        </div>
+        <div className='small-6 columns'>
+          <SelectField
+            fieldName='demographics.sexual_orientation'
+            label='Sexual Orientation'
+            options={sexualOrientationOptions}
+            validation={!demographics || (demographics && !demographics.sexual_orientation_other) ? validate.isPresent('Sexual Orientation is required') : ''} />
+        </div>
+        <div className='small-6 columns'>
+          <InputField
+            fieldName='demographics.sexual_orientation_other'
+            label='Sexual Orientation (if not listed)'
+            maxLength={maxLengthMap['sexual_orientation_other']}
+            validation={!demographics || (demographics && !demographics.sexual_orientation) ? validate.isPresent('Sexual Orientation is required') : ''} />
+        </div>
       </div>
-      <div className='small-6 columns'>
-        <SelectField
-          fieldName='demographics.race'
-          label='Race'
-          options={raceOptions}
-          validation={validate.isPresent('Race is required')} />
-      </div>
-      <div className='small-6 columns'>
-        <SelectField
-          fieldName='demographics.gender'
-          label='Gender'
-          options={genderOptions}
-          validation={!demographics || (demographics && !demographics.gender_other) ? validate.isPresent('Gender is required') : ''} />
-      </div>
-      <div className='small-6 columns'>
-        <InputField
-          fieldName='demographics.gender_other'
-          label='Gender Specify (if not listed)'
-          maxLength={maxLengthMap['gender_other']}
-          validation={!demographics || (demographics && !demographics.gender) ? validate.isPresent('Gender is required') : ''} />
-      </div>
-      <div className='small-6 columns'>
-        <SelectField
-          fieldName='demographics.sexual_orientation'
-          label='Sexual Orientation'
-          options={sexualOrientationOptions}
-          validation={!demographics || (demographics && !demographics.sexual_orientation_other) ? validate.isPresent('Sexual Orientation is required') : ''} />
-      </div>
-      <div className='small-6 columns'>
-        <InputField
-          fieldName='demographics.sexual_orientation_other'
-          label='Sexual Orientation (if not listed)'
-          maxLength={maxLengthMap['sexual_orientation_other']}
-          validation={!demographics || (demographics && !demographics.sexual_orientation) ? validate.isPresent('Sexual Orientation is required') : ''} />
-      </div>
+      <div className='row' />
     </div>
-    <div className='row' />
-  </div>
-)
+  )
+}
 
 export default DemographicInfoSection
