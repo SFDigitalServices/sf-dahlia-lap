29805f51236cca98b919597f833dbe5ce8a3878a
Logan Arnett <logan@loganarnett.com>
2019-07-25 11:13:25 -0500

    updating for shared browser and removing images and css formatting

--- spec/javascript/e2e/Login.e2e.js
+++ spec/javascript/e2e/Login.e2e.js
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer'
 import { HEADLESS, DEFAULT_E2E_TIME_OUT } from '../support/puppeteer/consts'
 import sharedSteps from '../support/puppeteer/steps/sharedSteps'
+import IgnoreImageAndCSSLoad from '../utils/IgnoreAssets'
 
 let browser
 
@@ -8,18 +9,18 @@ describe('Login/Logout', () => {
   test('lead header loads correctly', async () => {
     browser = await puppeteer.launch({ headless: HEADLESS })
     let page = await browser.newPage()
+    IgnoreImageAndCSSLoad(page)
 
     await page.goto('http://localhost:3000/')
     await page.waitForSelector('#root')
 
     const html = await page.$eval('.lead-header_title', e => e.innerHTML)
     expect(html).toBe('Welcome to DAHLIA Partners.')
-
-    await browser.close()
   }, DEFAULT_E2E_TIME_OUT)
 
   test('should sign out successfully', async () => {
     let page = await browser.newPage()
+    IgnoreImageAndCSSLoad(page)
 
     await sharedSteps.loginAsAgent(page)
 
---
+++ spec/javascript/utils/IgnoreAssets.js
@@ -0,0 +1,12 @@
+const IgnoreImageAndCSSLoad = async (page) => {
+  await page.setRequestInterception(true)
+
+  page.on('request', (req) => {
+    if (req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || req.resourceType() === 'image') {
+      return req.abort()
+    }
+    return req.continue()
+  })
+}
+
+export default IgnoreImageAndCSSLoad
