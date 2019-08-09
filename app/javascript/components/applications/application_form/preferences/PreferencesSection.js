f1244968d840b06673f43c40629f610717dfe933
Logan Arnett <logan@loganarnett.com>
2019-08-09 11:31:30 -0400

    updated preference section

--- app/javascript/components/applications/application_form/preferences/PreferencesSection.js
+++ app/javascript/components/applications/application_form/preferences/PreferencesSection.js
@@ -1,29 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { forEach, isEmpty } from 'lodash'
 import PreferenceForm from './PreferenceForm'
 import { naturalKeyFromPreference, getFullHousehold } from './utils'
 import { FieldArray } from 'react-final-form-arrays'
 
-const allPreferencesSelected = (form, listingPreferences) => {
-  if (form.getState().values && !isEmpty(form.getState().values.preferences) && listingPreferences) {
-    return form.getState().values.preferences.length === listingPreferences.length
-  }
-}
-
-const hasHouseholdMembers = (form) => {
-  let hasPrimaryApplicant = !isEmpty(form.getState().values) && !isEmpty(form.getState().values.applicant) && form.getState().values.applicant.first_name
-  let hasHouseholdMembers = !isEmpty(form.getState().values) && !isEmpty(form.getState().values.household_members)
-  return (hasHouseholdMembers || hasPrimaryApplicant)
-}
-
-const disableAddPreference = (form, listingPreferences) => {
-  return (allPreferencesSelected(form, listingPreferences) || !hasHouseholdMembers(form))
-}
-
-class PreferencesSection extends React.Component {
-  componentDidMount () {
-    const { form, editValues } = this.props
-
+const PreferencesSection = ({ form, editValues, listingPreferences }) => {
+  const loadOnMount = () => {
     let autofillPreferences = []
 
     if (!isEmpty(editValues) && editValues.preferences) {
@@ -38,43 +20,58 @@ class PreferencesSection extends React.Component {
     }
   }
 
-  render () {
-    const { form, listingPreferences } = this.props
-    const fullHousehold = getFullHousehold(form.getState().values)
+  useEffect(() => {
+    loadOnMount()
+  }, [])
 
-    return (
-      <div className='border-bottom margin-bottom--2x'>
-        <h3>Preferences</h3>
-        <FieldArray name='preferences'>
-          {({ fields }) =>
-            <React.Fragment>
-              { fields.map((name, i) => {
-                return (
-                  <div className='border-bottom margin-bottom--2x' key={name}>
-                    <PreferenceForm {...{i, name, form, listingPreferences, fullHousehold}} />
-                  </div>)
-              })
-              }
-              <div className='row'>
-                <div className='form-group'>
-                  <div className='small-4 columns'>
-                    <button
-                      onClick={() => form.mutators.push('preferences', {})}
-                      disabled={disableAddPreference(form, listingPreferences)}
-                      type='button'
-                      className='mb-4 mr-4 btn btn-success'
-                      id='add-preference-button'>
-                        + Add Preference
-                    </button>
-                  </div>
+  const allPreferencesSelected = () => {
+    if (form.getState().values && !isEmpty(form.getState().values.preferences) && listingPreferences) {
+      return form.getState().values.preferences.length === listingPreferences.length
+    }
+  }
+
+  const hasHouseholdMembers = () => {
+    let hasPrimaryApplicant = !isEmpty(form.getState().values) && !isEmpty(form.getState().values.applicant) && form.getState().values.applicant.first_name
+    let hasHouseholdMembers = !isEmpty(form.getState().values) && !isEmpty(form.getState().values.household_members)
+    return (hasHouseholdMembers || hasPrimaryApplicant)
+  }
+
+  const disableAddPreference = () => {
+    return (allPreferencesSelected() || !hasHouseholdMembers())
+  }
+
+  return (
+    <div className='border-bottom margin-bottom--2x'>
+      <h3>Preferences</h3>
+      <FieldArray name='preferences'>
+        {({ fields }) =>
+          <React.Fragment>
+            { fields.map((name, i) => {
+              return (
+                <div className='border-bottom margin-bottom--2x' key={name}>
+                  <PreferenceForm {...{i, name, form, listingPreferences, fullHousehold: getFullHousehold(form.getState().values)}} />
+                </div>)
+            })
+            }
+            <div className='row'>
+              <div className='form-group'>
+                <div className='small-4 columns'>
+                  <button
+                    onClick={() => form.mutators.push('preferences', {})}
+                    disabled={disableAddPreference()}
+                    type='button'
+                    className='mb-4 mr-4 btn btn-success'
+                    id='add-preference-button'>
+                      + Add Preference
+                  </button>
                 </div>
               </div>
-            </React.Fragment>
-          }
-        </FieldArray>
-      </div>
-    )
-  }
+            </div>
+          </React.Fragment>
+        }
+      </FieldArray>
+    </div>
+  )
 }
 
 export default PreferencesSection
