41ad9fbb4c50a7854a356c74ad14393250b9c5f1
Logan Arnett <logan@loganarnett.com>
2019-08-10 11:27:33 -0400

    updating layouts and eager loading

--- app/javascript/components/layouts/CardLayout.js
+++ app/javascript/components/layouts/CardLayout.js
@@ -1,41 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import PageHeader from '../organisms/PageHeader'
 import TabsSection from '../organisms/TabsSection'
 import TabCard from '../organisms/TabCard'
 import AppCard from '../molecules/AppCard'
 
-class CardLayout extends React.Component {
-  componentWillMount = () => {
+const CardLayout = ({ children, pageHeader, tabSection, toolbar }) => {
+  useEffect(() => {
     document.body.classList.add('bg-snow')
-  }
+  }, [])
 
-  render () {
-    const { children, pageHeader, tabSection, toolbar } = this.props
-
-    if (tabSection && !tabSection.currentUrl) { tabSection.currentUrl = window.location.pathname }
-
-    return (
-      <React.Fragment>
-        <PageHeader {...pageHeader} background='snow' />
-        { tabSection
-          ? (
-            <TabsSection {...tabSection} background='snow'>
+  if (tabSection && !tabSection.currentUrl) { tabSection.currentUrl = window.location.pathname }
+  return (
+    <React.Fragment>
+      <PageHeader {...pageHeader} background='snow' />
+      { tabSection
+        ? (
+          <TabsSection {...tabSection} background='snow'>
+            <AppCard>{children}</AppCard>
+          </TabsSection>
+        )
+        : (
+          <React.Fragment>
+            { toolbar && toolbar() }
+            <TabCard padding>
               <AppCard>{children}</AppCard>
-            </TabsSection>
-          )
-          : (
-            <React.Fragment>
-              { toolbar && toolbar() }
-              <TabCard padding>
-                <AppCard>{children}</AppCard>
-              </TabCard>
-            </React.Fragment>
-          )
-        }
-      </React.Fragment>
-    )
-  }
+            </TabCard>
+          </React.Fragment>
+        )
+      }
+    </React.Fragment>
+  )
 }
 
 export default CardLayout
--- app/javascript/components/layouts/TableLayout.js
+++ app/javascript/components/layouts/TableLayout.js
@@ -1,34 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import PageHeader from '../organisms/PageHeader'
 import TabsSection from '../organisms/TabsSection'
 import TabCard from '../organisms/TabCard'
 
-class TableLayout extends React.Component {
-  componentWillMount = () => {
+const TableLayout = ({ children, pageHeader, tabSection }) => {
+  useEffect(() => {
     document.body.classList.add('bg-white')
-  }
+  }, [])
 
-  render () {
-    const { children, pageHeader, tabSection } = this.props
-    return (
-      <React.Fragment>
-        <PageHeader {...pageHeader} background='dust' />
-        { tabSection
-          ? (
-            <TabsSection {...tabSection} background='dust' padding>
-              {children}
-            </TabsSection>
-          )
-          : (
-            <TabCard padding>
-              {children}
-            </TabCard>
-          )
-        }
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <PageHeader {...pageHeader} background='dust' />
+      { tabSection
+        ? (
+          <TabsSection {...tabSection} background='dust' padding>
+            {children}
+          </TabsSection>
+        )
+        : (
+          <TabCard padding>
+            {children}
+          </TabCard>
+        )
+      }
+    </React.Fragment>
+  )
 }
 
 export default TableLayout
