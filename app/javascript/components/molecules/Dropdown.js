f5309ad04f8c261c9a50112b8221a1042dc87c1f
Logan Arnett <logan@loganarnett.com>
2019-08-12 11:35:13 -0400

    updating dropdown component

--- app/javascript/components/molecules/Dropdown.js
+++ app/javascript/components/molecules/Dropdown.js
@@ -1,114 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { find } from 'lodash'
 import classNames from 'classnames'
 
 import DropdownMenu from '../molecules/DropdownMenu'
 import DropdownMenuMultiSelect from '../molecules/DropdownMenuMultiSelect'
 
-class Dropdown extends React.Component {
-  state = { expanded: false, style: { top: 40, left: 0 } }
+const dropdownStyle = { top: 40, left: 0 }
 
-  componentDidMount () {
-    document.addEventListener('mousedown', this.handleClickOutside)
-    document.addEventListener('keydown', this.onEscapeHandler)
-  }
+const Dropdown = ({
+  prompt,
+  items,
+  value,
+  styles,
+  buttonClasses,
+  wrapperClasses,
+  menuClasses = [],
+  disabled,
+  multiple,
+  onChange
+}) => {
+  const [expanded, setExpanded] = useState(false)
+  const wrapperRef = useRef(null)
+  const buttonRef = useRef(null)
+  const DropdownComponent = multiple ? DropdownMenuMultiSelect : DropdownMenu
 
-  componentWillUnmount () {
-    document.removeEventListener('mousedown', this.handleClickOutside)
-    document.removeEventListener('keydown', this.onEscapeHandler)
-  }
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener('keydown', onEscapeHandler)
 
-  toggleExpand = (e) => {
-    this.setState((prevState) => ({ expanded: !prevState.expanded }))
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+      document.removeEventListener('keydown', onEscapeHandler)
+    }
+  }, [])
+
+  const toggleExpand = (e) => {
+    setExpanded(!expanded)
     // We need this to avoid triggering the collapse function from Foundation
     e.stopPropagation()
   }
 
-  componentClickHandler = (e) => {
+  const componentClickHandler = (e) => {
     // We want to hide the dropdown menu only when we click in the component but outside the menu
-    if (this.wrapperRef === e.target) { this.setState({ expanded: false }) }
+    if (wrapperRef === e.target) { setExpanded(false) }
   }
 
-  onChangeHandler = (value, label) => {
-    if (!this.props.multiple) { this.setState({expanded: false}) }
-    if (!this.props.multiple) { this.buttonRef.focus() }
-    this.props.onChange && this.props.onChange(value, label)
+  const onChangeHandler = (value, label) => {
+    if (!multiple) {
+      setExpanded(false)
+      buttonRef.focus()
+    }
+    onChange && onChange(value, label)
   }
 
-  menu = (classes) => {
-    const { items, value, multiple } = this.props
-    if (this.state.expanded) {
-      if (multiple) {
-        return (
-          <DropdownMenuMultiSelect
-            style={this.state.style}
-            onChange={this.onChangeHandler}
+  const handleClickOutside = (event) => {
+    if (wrapperRef && !wrapperRef.current.contains(event.target)) {
+      setExpanded(false)
+    }
+  }
+
+  const onEscapeHandler = (event) => {
+    if (event.keyCode === 27) { setExpanded(false) }
+  }
+
+  const selectedItem = find(items, { value })
+
+  return (
+    <div
+      className={classNames('dropdown', wrapperClasses)}
+      onClick={componentClickHandler}
+      ref={wrapperRef}
+      style={styles}>
+      <button
+        aria-expanded={expanded ? 'true' : 'false'}
+        onClick={toggleExpand}
+        ref={buttonRef}
+        className={`button dropdown-button has-icon--right text-align-left ${buttonClasses ? buttonClasses.join(' ') : ''}`}
+        type='button'
+        disabled={disabled}>
+        <span className='ui-icon ui-small'>
+          <svg>
+            <use xlinkHref='#i-arrow-down' />
+          </svg>
+        </span>
+        {selectedItem ? selectedItem.label : prompt}
+      </button>
+      <div className='dropdown-menu-wrapper' aria-hidden={expanded ? 'false' : 'true'} role='menu'>
+        {expanded && (
+          <DropdownComponent
+            style={dropdownStyle}
+            onChange={onChangeHandler}
             values={value}
             items={items}
-            classes={classes} />
-        )
-      } else {
-        return (
-          <DropdownMenu
-            style={this.state.style}
-            onChange={this.onChangeHandler}
-            value={value}
-            items={items}
-            classes={classes} />
-        )
-      }
-    }
-  }
-
-  handleClickOutside = (event) => {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      this.setState({expanded: false})
-    }
-  }
-
- onEscapeHandler = (event) => {
-   if (event.keyCode === 27) { this.setState({expanded: false}) }
- }
-
- render () {
-   const {
-     prompt,
-     items,
-     value,
-     styles,
-     buttonClasses,
-     wrapperClasses,
-     menuClasses = [],
-     disabled
-   } = this.props
-   const selectedItem = find(items, { value: value })
-
-   return (
-     <div
-       className={classNames('dropdown', wrapperClasses)}
-       onClick={this.componentClickHandler}
-       ref={(node) => { this.wrapperRef = node }}
-       style={styles}>
-       <button
-         aria-expanded={this.state.expanded ? 'true' : 'false'}
-         onClick={this.toggleExpand}
-         ref={(node) => { this.buttonRef = node }}
-         className={`button dropdown-button has-icon--right text-align-left ${buttonClasses ? buttonClasses.join(' ') : ''}`}
-         type='button'
-         disabled={disabled}>
-         <span className='ui-icon ui-small'>
-           <svg>
-             <use xlinkHref='#i-arrow-down' />
-           </svg>
-         </span>
-         {selectedItem ? selectedItem.label : prompt}
-       </button>
-       <div className='dropdown-menu-wrapper' aria-hidden={this.state.expanded ? 'false' : 'true'} role='menu'>
-         {this.menu(menuClasses)}
-       </div>
-     </div>
-   )
- }
+            classes={menuClasses} />
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Dropdown
