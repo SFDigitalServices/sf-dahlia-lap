98ee489822860c11d86381e9b9ea5da8d4bfcb3e
Marcin Jedras <mjjedras@gmail.com>
2019-09-06 22:58:01 +0200

    [#167210410] Fix listings mocks

--- spec/javascript/components/applications/ListingApplicationsPage.test.js
+++ spec/javascript/components/applications/ListingApplicationsPage.test.js
@@ -37,7 +37,7 @@ const applications = [
 
 describe('ListingApplicationsPage', () => {
   test('should render succesfully', () => {
-    const listing = { id: 1, mame: 'xxxxListing' }
+    const listing = { id: 1, name: 'xxxxListing' }
 
     const wrapper = renderer.create(
       <ListingApplicationsPage
