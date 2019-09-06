eee9fdd5d97bc530bb453235188b5c93beb5bfd7
Marcin Jedras <mjjedras@gmail.com>
2019-09-07 00:14:59 +0200

    [#167210410] Fix listing view fields mapping

--- app/javascript/components/listings/fields.js
+++ app/javascript/components/listings/fields.js
@@ -1,12 +1,12 @@
 export const detailsFields = [
-  { field: 'Owner.Name', label: 'Owner' },
-  'Name',
-  { field: 'Account.Name', label: 'Account' },
-  'Application_Due_Date',
-  { field: 'In_Lottery', label: 'Applications in Lottery' },
-  'Status',
-  'Lottery_Winners',
-  'Lottery_Results'
+  { field: 'owner', label: 'Owner' },
+  'name',
+  { field: 'account', label: 'Account' },
+  'application_due_date',
+  { field: 'in_lottery', label: 'Applications in Lottery' },
+  'status',
+  'lottery_winners',
+  'lottery_results'
 ]
 
 export const buildingInformationFields = [
