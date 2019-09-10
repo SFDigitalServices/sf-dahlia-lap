5f1b73113975de31e255102b1f007dcceafb5caa
Andrea Egan <aegan526@gmail.com>
2019-09-10 14:55:30 -0700

    [#168356932] Update styles to darker alert tint
    toolkit.scss update pending pattern lib PR being merged

--- app/javascript/components/lease_ups/LeaseUpApplicationsTable.js
+++ app/javascript/components/lease_ups/LeaseUpApplicationsTable.js
@@ -36,7 +36,7 @@ const PreferenceRankCell = ({cell}) => {
     return (
       <div>
         <span className='rt-td-label-rank t-semis'>{cell.original.preference_rank}</span>
-        <span className='rt-td-label-invalid t-semis'>{cell.original.preference_record_type} Invalid</span>
+        <span className='rt-td-label-invalid t-semis'>Invalid {cell.original.preference_record_type}</span>
       </div>
     )
   } else {
@@ -86,7 +86,7 @@ const LeaseUpApplicationsTable = ({ listingId, dataSet, onLeaseUpStatusChange, o
     const trClassName = classNames(
       'rt-tr-status',
       statusClassName,
-      { 'is-invalid is-disqualified': rowInfo && isInvalid(rowInfo.original) }
+      { 'is-invalid': rowInfo && isInvalid(rowInfo.original) }
     )
 
     return {
