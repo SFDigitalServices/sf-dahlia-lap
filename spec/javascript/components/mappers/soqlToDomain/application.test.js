4583467c0b709ca1e2bee6e6d5793b4f97cefe11
Andrea Egan <aegan526@gmail.com>
2019-09-17 16:41:16 -0700

    [#168356932] Add test for removal of id in alt contact soqlToDomain

--- spec/javascript/components/mappers/soqlToDomain/application.test.js
+++ spec/javascript/components/mappers/soqlToDomain/application.test.js
@@ -1,7 +1,7 @@
 // TODO: Expand these tests to test all fields in application.
 import { mapApplication } from '~/components/mappers/soqlToDomain'
 import soqlApplication from '../../../fixtures/application'
-import { forEach } from 'lodash'
+import { forEach, cloneDeep } from 'lodash'
 
 describe('mapApplication', () => {
   test('it should create demographics based on primary applicant values', async () => {
@@ -44,5 +44,43 @@ describe('mapApplication', () => {
     forEach(expectedDomainCurrencyValues, (value, domainFieldName) => {
       expect(mappedApplication[domainFieldName]).toEqual(value)
     })
+  })
+
+  test('it maps alternate contact as expected', () => {
+    const mockSoqlApplication = cloneDeep(soqlApplication)
+
+    // Expect that alternate contact does not include id
+    const expectedAlternateContact = {
+      'first_name': 'Federic',
+      'middle_name': 'Daaaa',
+      'last_name': 'dayan',
+      'phone_type': 'Cell',
+      'phone': '9954449943',
+      'email': 'fede@eee.com',
+      'agency_name': null,
+      'alternate_contact_type': 'Friend',
+      'alternate_contact_type_other': null,
+      'mailing_address': '123 Alternate Mailing St, SF Mailing, CA, 94105-1804',
+      'mailing_street': '123 Alternate Mailing St',
+      'mailing_city': 'SF Mailing',
+      'mailing_state': 'CA',
+      'mailing_zip_code': '94105-1804',
+      // Fields below are automatically added by application member mapping, are not used
+      'primary_language': undefined,
+      'relationship_to_applicant': undefined,
+      'residence_address': undefined,
+      'second_phone': undefined,
+      'second_phone_type': undefined,
+      'state': undefined,
+      'street': undefined,
+      'zip_code': undefined,
+      'city': undefined,
+      'date_of_birth': null,
+      'marital_status': undefined,
+      'name': undefined
+    }
+
+    const mappedApplication = mapApplication(mockSoqlApplication)
+    expect(mappedApplication['alternate_contact']).toEqual(expectedAlternateContact)
   })
 })
