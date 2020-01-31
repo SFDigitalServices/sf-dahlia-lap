be1e6b775acb9641cd8f362d83657455bbcceb4e
Logan Arnett <logan@loganarnett.com>
2020-01-31 12:28:10 -0500

    removed unused function

--- spec/javascript/utils/utils.test.js
+++ spec/javascript/utils/utils.test.js
@@ -1,19 +1,4 @@
-import { currencyToFloat } from '~/utils/utils'
 import { convertCurrency } from '~/utils/form/validations'
-
-describe('currencyToFloat', () => {
-  test('should parse currency values as expected', async () => {
-    expect(currencyToFloat(1000)).toEqual(1000)
-    expect(currencyToFloat('1000')).toEqual(1000)
-    expect(currencyToFloat('1000.00')).toEqual(1000.00)
-    expect(currencyToFloat('$1000.00')).toEqual(1000.00)
-    expect(currencyToFloat('$1,000.00')).toEqual(1000.00)
-    expect(currencyToFloat(null)).toEqual(null)
-    expect(currencyToFloat(undefined)).toEqual(null)
-    expect(currencyToFloat('')).toEqual(null)
-    expect(currencyToFloat(0)).toEqual(0)
-  })
-})
 
 const defaultFormObject = {
   applicant: {
