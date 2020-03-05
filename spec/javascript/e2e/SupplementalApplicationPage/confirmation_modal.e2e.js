88d496b33d8ed9b5c6c0f080af9d9077a905bb2d
Logan Arnett <logan@loganarnett.com>
2020-03-05 13:37:41 -0500

    Updating for reset form touched on save #171364152 (#330)
    * updating for resetting touched
    
    * updating test for disabled submission

--- spec/javascript/e2e/SupplementalApplicationPage/confirmation_modal.e2e.js
+++ spec/javascript/e2e/SupplementalApplicationPage/confirmation_modal.e2e.js
@@ -1,5 +1,6 @@
 import utils from '../../support/puppeteer/utils'
 import sharedSteps from '../../support/puppeteer/steps/sharedSteps'
+import supplementalApplicationSteps from '../../support/puppeteer/steps/supplementalApplicationSteps'
 import {
   LEASE_UP_LISTING_APPLICATION_ID,
   DEFAULT_E2E_TIME_OUT
@@ -36,5 +37,27 @@ describe('SupplementalApplicationPage confirm modal', () => {
     expect(page.url()).toBe(`http://localhost:3000/applications/${LEASE_UP_LISTING_APPLICATION_ID}?lease_up=true`)
 
     await testBrowser.close()
+  }, DEFAULT_E2E_TIME_OUT)
+  test('should not pop up if the form was submitted successfully', async () => {
+    let { browser, page } = await SetupBrowserAndPage(null, true)
+    testBrowser = browser
+
+    await sharedSteps.loginAsAgent(page)
+    await sharedSteps.goto(page, `/applications/${LEASE_UP_LISTING_APPLICATION_ID}/supplementals`)
+
+    // Change value for ada priorities, mobility impairments
+    await page.click('input#form-has_ada_priorities_selected\\.mobility_impairments')
+
+    await supplementalApplicationSteps.savePage(page)
+
+    const isDisabled = await page.$eval('#save-supplemental-application', el => el.disabled)
+    expect(isDisabled).toBeTruthy()
+    // wait for full form submission
+    await page.waitForSelector('#save-supplemental-application:not([disabled])')
+    // Click on Original Short Form Application Tab
+    await page.click('.tabs li:nth-child(1)')
+
+    const hasConfirmModal = await utils.isPresent(page, '#leave-confirmation-modal')
+    expect(hasConfirmModal).toBe(false)
   }, DEFAULT_E2E_TIME_OUT)
 })
