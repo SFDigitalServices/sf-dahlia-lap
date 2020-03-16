5a65caff3ced3aab088609d08fc35231593267a0
Marcin Jedras <mjjedras@gmail.com>
2020-03-16 17:59:30 +0100

    Make app rename script more secure

--- lib/update_app_name.rb
+++ lib/update_app_name.rb
@@ -2,10 +2,11 @@
 
 # Service for updating Heroku review app's name, to keep app name convension: 'dahlia-lap-full-pr-XXX'.
 class UpdateAppName
-  SKIPPED_APPS = %w[dahlia-lap-full dahlia-lap-nonepic dahlia-lap-qa dahlia-lap-production dahlia-lap-preprod].freeze
+  PIPELINE_ID = 'c3953280-7b7a-42b2-8ec6-7815ec32c2d0'
 
   def initialize
-    @heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
+    @fallback_id = 0
+    @heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'] || '7b1680c7-b954-4909-b55d-95c2d44a3615')
   end
 
   def self.call
@@ -14,15 +15,21 @@ class UpdateAppName
 
   def call
     apps = @heroku.app.list
-
-    @lap_apps = apps.select { |app| app['name'] =~ /dahlia-lap-/ }
-    apps = @lap_apps.reject { |app| SKIPPED_APPS.any? { |name| name == app['name'] } }
-    apps.reject! { |app| app['name'] =~ /dahlia-lap-full-/ }
-
-    return if apps.empty?
+    @review_apps = @heroku.review_app.list(PIPELINE_ID)
+    @lap_apps = []
+    needs_rename = []
 
     apps.each do |app|
-      review_app = @heroku.review_app.get_review_app_by_app_id(app['id'])
+      if @review_apps.any? { |review_app| review_app['app']['id'] == app['id'] }
+        @lap_apps << app
+        needs_rename << app if app['name'] !~ /dahlia-lap-full-/
+      end
+    end
+
+    return if needs_rename.empty?
+
+    needs_rename.each do |app|
+      review_app = @review_apps.find { |ra| ra['app']['id'] == app['id'] }
       update_app(review_app)
     end
   end
@@ -32,26 +39,24 @@ class UpdateAppName
   def update_app(app)
     return if app.blank?
 
-    puts "Updating app #{app['id']}"
-
-    puts @heroku.app.update(app['app']['id'], name: new_app_name(app))
+    @heroku.app.update(app['app']['id'], name: new_app_name(app))
   end
 
   def new_app_name(app)
     if app['pr_number'].present?
       "dahlia-lap-full-pr-#{app['pr_number']}"
     else
-      failback_name
+      fallback_name
     end
   end
 
   def fallback_name
-    name = 'dahlia-lap-full-failback-1'
-    failback_apps = @lap_apps.select { |app| app['name'] =~ /dahlia-lap-full-failback-/ }
-    index = 1
-    while failback_apps.any? { |app| app['name'] == name }
-      index += 1
-      name = "dahlia-lap-full-failback-#{index}"
+    @fallback_id += 1
+    name = "dahlia-lap-full-fallback-#{@fallback_id}"
+    fallback_apps ||= @lap_apps.select { |app| app['name'] =~ /dahlia-lap-full-fallback-/ }
+    while fallback_apps.any? { |app| app['name'] == name }
+      @fallback_id += 1
+      name = "dahlia-lap-full-fallback-#{@fallback_id}"
     end
     name
   end
