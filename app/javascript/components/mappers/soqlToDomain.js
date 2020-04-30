18f5cce86579e262fc360c50b9e6a1e542a08f5b
Logan Arnett <logan@loganarnett.com>
2020-04-30 12:16:16 -0400

    removing mappers

--- app/javascript/components/mappers/soqlToDomain.js
+++
@@ -1,4 +0,0 @@
-export { mapListing } from './soqlToDomain/listing'
-export { mapUnit } from './soqlToDomain/unit'
-export { mapFlaggedApplication } from './soqlToDomain/flagged_application'
-export { mapFieldUpdateComment } from './soqlToDomain/field_update_comment'
--- app/javascript/components/mappers/soqlToDomain/listing_preference.js
+++
@@ -1,5 +0,0 @@
-export const mapListingPreference = (value) => {
-  return {
-    record_type_for_app_preferences: value.Record_Type_For_App_Preferences
-  }
-}
