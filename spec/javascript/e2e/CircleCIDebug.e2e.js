8791ab8e93fe2719596a1a2a36162eb916680624
Andrea Egan <andrea.egan@sfgov.org>
2020-05-07 14:46:49 -0700

    Add login to test e2e test

--- spec/javascript/e2e/CircleCIDebug.e2e.js
+++ spec/javascript/e2e/CircleCIDebug.e2e.js
@@ -5,7 +5,7 @@ describe('CircleCI Test', () => {
   test('lead header loads correctly', async () => {
     let browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
 
-    console.log('browser', browser)
+    // console.log('browser', browser)
     let version = await browser.version()
     console.log('browser version', version)
     let page = await browser.newPage()
@@ -20,5 +20,30 @@ describe('CircleCI Test', () => {
 
     const html = await page.$eval('.lead-header_title', e => e.innerHTML)
     expect(html).toBe('Welcome to DAHLIA Partners.')
+
+    // Sign in
+    const navigationPromise = page.waitForNavigation()
+    // Clicking the link will indirectly cause a navigation
+    await page.click('.sign-in-btn')
+    // The navigationPromise resolves after navigation has finished
+    await navigationPromise
+    let loginUrl = await page.url()
+    console.log('login page url', loginUrl)
+    let loginPageContent = await page.content()
+    console.log('login page content', loginPageContent)
+
+    // Salesforce login
+    await page.waitForSelector('#username_container')
+    await page.type('#username', process.env.E2E_SALESFORCE_USERNAME)
+    await page.type('#password', process.env.E2E_SALESFORCE_PASSWORD)
+    await page.click('#Login')
+
+    await page.waitForNavigation()
+
+    loginUrl = await page.url()
+    console.log('login page url after 2nd wait for navigation', loginUrl)
+    loginPageContent = await page.content()
+    console.log('login page content after 2nd wait for navigation', loginPageContent)
+    await page.waitForSelector('#root')
   }, DEFAULT_E2E_TIME_OUT)
 })
