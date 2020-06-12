8f628702c79e8a99d6102eb10b8dac6504bf3778
Andrea Egan <andrea.egan@sfgov.org>
2020-06-12 15:17:20 -0700

    Replace semaphore with CircleCI env var updates

--- refresh.sh
+++ refresh.sh
@@ -15,7 +15,6 @@
 
 # Argument defaults
 env_file=".env"
-semaphore_secret="sf-dahlia-partners-full"
 
 while getopts ":f::s::h" opt; do
   case $opt in
@@ -23,14 +22,14 @@ while getopts ":f::s::h" opt; do
       echo "Usage:"
       echo "    refresh.sh -h                           Display this help message."
       echo "    refresh.sh -f <environment file>        Specify an environment file to load from, defaults to .env."
-      echo "    refresh.sh -s <semaphore secret name>   Specify a Semaphore secret name to update, defaults to sf-dahlia-partners-full."
+      echo "    refresh.sh -c <circle ci token>         Provide a CircleCI token."
       exit 0
       ;;
     f )
       env_file=$OPTARG
       ;;
-    s )
-      semaphore_secret=$OPTARG
+    c )
+      circle_ci_token=$OPTARG
       ;;
     \? ) echo "Usage: cmd [-h] [-f]"
       ;;
@@ -70,22 +69,25 @@ done
 echo "Heroku update complete"
 
 
-echo "Starting Semaphore credential update for $semaphore_secret"
+echo "Starting CircleCI credential update"
+BASE_CIRCLECI_URL="https://circleci.com/api/v1.1/project/github/SFDigitalServices/sf-dahlia-lap/envvar"
 
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_PASSWORD
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_SECURITY_TOKEN
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_CLIENT_SECRET
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_CLIENT_ID
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name SALESFORCE_INSTANCE_URL
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name COMMUNITY_LOGIN_URL
-sem secrets:env-vars:remove exygy/$semaphore_secret  --name E2E_SALESFORCE_PASSWORD
+# Delete existing env vars
+curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_PASSWORD?circle-token=$circle_ci_token
+curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_SECURITY_TOKEN?circle-token=$circle_ci_token
+curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_CLIENT_SECRET?circle-token=$circle_ci_token
+curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_CLIENT_ID?circle-token=$circle_ci_token
+curl -X DELETE $BASE_CIRCLECI_URL/SALESFORCE_INSTANCE_URL?circle-token=$circle_ci_token
+curl -X DELETE $BASE_CIRCLECI_URL/COMMUNITY_LOGIN_URL?circle-token=$circle_ci_token
+curl -X DELETE $BASE_CIRCLECI_URL/E2E_SALESFORCE_PASSWORD?circle-token=$circle_ci_token
 
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_PASSWORD --content "$SALESFORCE_PASSWORD"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_SECURITY_TOKEN --content "$SALESFORCE_SECURITY_TOKEN"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_CLIENT_SECRET --content "$SALESFORCE_CLIENT_SECRET"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_CLIENT_ID --content "$SALESFORCE_CLIENT_ID"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name SALESFORCE_INSTANCE_URL --content "$SALESFORCE_INSTANCE_URL"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name COMMUNITY_LOGIN_URL --content "$COMMUNITY_LOGIN_URL"
-sem secrets:env-vars:add exygy/$semaphore_secret  --name E2E_SALESFORCE_PASSWORD --content "$E2E_SALESFORCE_PASSWORD"
+# Create new env vars
+curl -X POST --header "Content-Type: application/json" -d '{"name": "SALESFORCE_PASSWORD", "value": "$SALESFORCE_PASSWORD"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+curl -X POST --header "Content-Type: application/json" -d '{"name": "SALESFORCE_SECURITY_TOKEN", "value": "$SALESFORCE_SECURITY_TOKEN"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+curl -X POST --header "Content-Type: application/json" -d '{"name": "SALESFORCE_CLIENT_SECRET", "value": "$SALESFORCE_CLIENT_SECRET"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+curl -X POST --header "Content-Type: application/json" -d '{"name": "SALESFORCE_CLIENT_ID", "value": "$SALESFORCE_CLIENT_ID"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+curl -X POST --header "Content-Type: application/json" -d '{"name": "SALESFORCE_INSTANCE_URL", "value": "$SALESFORCE_INSTANCE_URL"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+curl -X POST --header "Content-Type: application/json" -d '{"name": "COMMUNITY_LOGIN_URL", "value": "$COMMUNITY_LOGIN_URL"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
+curl -X POST --header "Content-Type: application/json" -d '{"name": "E2E_SALESFORCE_PASSWORD", "value": "$E2E_SALESFORCE_PASSWORD"}' $BASE_CIRCLECI_URL?circle-token=$circle_ci_token
 
-echo "Credentials updated for Semaphore"
+echo "Credentials updated for CircleCI"
