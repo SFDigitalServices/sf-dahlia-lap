af28c483e51d3a6919b63924cca3b37ecf5be580
Marcin Jedras <mjjedras@gmail.com>
2020-06-30 21:49:35 +0200

    Fixes for short form put request test

--- spec/controllers/api/v1/short_form_controller_spec.rb
+++ spec/controllers/api/v1/short_form_controller_spec.rb
@@ -4,39 +4,25 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ShortFormController, type: :controller do
   login_admin
-  pre_lottery_listing_id = 'a0W0P00000F8YG4UAN' # Automated Test Listing
-  new_application = {
-    application_submission_type: 'Paper',
-    application_submitted_date: '2019-03-12',
-    status: 'Submitted',
-    listing_id: pre_lottery_listing_id,
-    annual_income: 110_000,
-    application_language: 'English',
+  application = {
     applicant: {
-      first_name: 'Test',
-      last_name: 'Supp app test',
-      email: 'eee@eeee.com',
-      date_of_birth: { day: 1, year: 1950, month: 1 },
-      address: '123 MAIN ST',
-      city: 'SAN FRANCISCO',
-      state: 'CA',
-      zip_code: '94105-1804',
+      marital_status: 'Single',
+      id: 'a0n0P00000DowdHQAR'
     },
-    household_members: [
-      {
-        first_name: 'member',
-        last_name: 'test',
-        date_of_birth: { day: 11, year: 1976, month: 6 },
-      },
-    ],
-    shortForm_preferences: [],
+    has_military_service: 'Yes',
+    reserved_senior: 'Yes',
+    has_ada_priorities_selected: {vision_impairments: true},
+    number_of_dependents: 2,
+    number_of_seniors: 2,
+    confirmed_household_annual_income: 123,
+    id: 'a0o0P00000JQawYQAT'
   }
 
   describe '#submit' do
     describe 'application to pre-lottery listing' do
       it 'receives a successful response from Salesforce' do
         VCR.use_cassette('api/v1/short-form/submit/pre-lottery/success') do
-          put :submit, params: { application: new_application }
+          put :submit, params: { application: application }
         end
         expect(response).to have_http_status(:success)
         json = JSON.parse(response.body)
@@ -46,7 +32,7 @@ RSpec.describe Api::V1::ShortFormController, type: :controller do
       describe 'with supplemental param' do
         it 'receives a successful response with updated application from Salesforce' do
           VCR.use_cassette('api/v1/short-form/submit/pre-lottery/with-supplemental-param') do
-            put :submit, params: { application: new_application, supplemental: true }
+            put :submit, params: { application: application, supplemental: true }
           end
           expect(response).to have_http_status(:success)
           json = JSON.parse(response.body)
