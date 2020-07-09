0da4f7cfbfdd50e55ac2c43b9702abf158551075
James Wills <james.wills@sfgov.org>
2020-07-09 14:42:54 -0700

    Create scripts to speed up release process

---
+++ create_release_branch.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This creates a new release branch off of production, merges it with main,
+# and creates a new pull request for that branch.
+
+skip_branch_creation='false'
+
+while getopts ":s::h:" opt; do
+  case $opt in
+    h )
+      echo "Usage:"
+      echo "    refresh.sh -h                           Display this help message."
+      echo "    refresh.sh -s                           Skip creating a new branch and merging it with the latest main. Use this when you needed to manually complete a merge."
+      exit 0
+      ;;
+    s )
+      skip_branch_creation='true'
+      ;;
+    \? ) echo "Usage: cmd [-h] [-s]"
+      ;;
+  esac
+done
+
+# Ensure working directory in version branch clean
+git update-index -q --refresh
+if ! git diff-index --quiet HEAD --; then
+  echo "Working directory not clean, please commit your changes first"
+  exit
+fi
+
+
+formatted_date=$(date +'%m-%d-%Y')
+branch_name="release-$formatted_date"
+
+if ${OPT_X}; then
+    echo "Skipping creating a new branch. Checking out $branch_name."
+    git checkout $branch_name
+else
+  echo "Checking out new branch off production: $branch_name"
+  git checkout main && git pull --rebase origin main
+
+  echo "Checking out new branch off production: $branch_name"
+  git checkout -b $branch_name origin/production
+
+  echo "Merging $branch_name with main."
+  git merge main --ff-only --no-edit
+
+  if [ $? -ne 0 ]; then
+      echo "Merge failed because it wasn't a simple fast-forward."
+      echo "  To continue, do the following: "
+      echo "    1. Do a manual merge: Run `git merge main` from branch: $branch_name. You may need to address merge conflicts."
+      echo "    2. Run this script with the -s (skip branch creation) flag. Like `yarn create_release_branch -s`"
+      exit 1
+  fi
+fi
+
+echo "Pushing branch to Github and opening the compare view in browser."
+
+git push origin -u $branch_name
+open "https://github.com/SFDigitalServices/sf-dahlia-lap/compare/production...$branch_name?expand=1";
---
+++ print_release_info.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# This prints release information to the console that you can easily copy and paste into the
+# release tracker and Github release description.
+
+# What you need before running this script:
+# 1. Generate a personal access token from github by going to github -> profile settings -> developer settings -> personal access token -> Generate new token.
+while getopts ":h::u::t:" opt; do
+  case $opt in
+    h )
+      echo "Usage:"
+      echo "    refresh.sh -h                           Display this help message."
+      echo "    refresh.sh -u <github-username>         Provide your Github username."
+      echo "    refresh.sh -t <github token>            Provide your Github personal access token. Generate a new token from Github by going to profile settings -> developer settings -> personal access token -> Generate new token."
+      exit 0
+      ;;
+    u )
+      username=$OPTARG
+      ;;
+    t )
+      github_token=$OPTARG
+      ;;
+    \? ) echo "Usage: cmd [-h] [-u] <github username> [-t] <github token>"
+      ;;
+  esac
+done
+
+if [ -z "$username" ]; then
+  echo "Must provide a github username."
+  exit
+fi
+
+if [ -z "$github_token" ]; then
+  echo "Must provide a github token. Generate a new token from Github by going to profile settings -> developer settings -> personal access token -> Generate new token."
+  exit
+fi
+
+bugs=()
+chores=()
+features=()
+other=()
+
+commitmessages=$(git log --oneline origin/production...)
+
+numcommits=$(echo "$commitmessages" | wc -l)
+echo "$commitmessages"
+echo "Fetching the branch names for each PR for $numcommits commits"
+while IFS= read -r line; do
+  echo "Reading $line"
+  pr=$(echo "$line" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/\\4/')
+
+  re='^[0-9]+$'
+  if ! [[ $pr =~ $re ]] ; then
+    other+=( "$line" )
+  else
+    prgeturl="https://api.github.com/repos/SFDigitalServices/sf-dahlia-lap/pulls/$pr"
+    type=$(curl -u $username:$github_token -s "$prgeturl" | jq .head.ref | cut -d'/' -f 1 | sed -e 's/^"//' -e 's/"$//')
+    sleep .1
+
+    if [[ "$type" = "bug" ]] || [[ "$type" = "bugs" ]]; then
+      bugs+=( "$line" )
+    elif [[ "$type" = "feature" ]] || [[ "$type" = "features" ]]; then
+      features+=( "$line" )
+    elif [[ "$type" = "chore" ]] || [[ "$type" = "chores" ]]; then
+      chores+=( "$line" )
+    else
+      other+=( "$line" )
+    fi
+  fi
+
+done <<<"$commitmessages"
+
+echo "
+
+
+---- Printing google sheets output ----"
+echo "Release tracker: https://docs.google.com/spreadsheets/d/1EUvw2ugaFprt8FxlCUa1yWATSn0KKo4FyRfBJWUwE3M/edit#gid=1500049656"
+
+echo "Paste the following into the release tracker on google sheets:"
+
+echo "
+Features"
+for t in "${features[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/\\2\t\\3\t=IF\\(not\\(isblank\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\)\\)\\), HYPERLINK\\(CONCAT\\("https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/",SUBSTITUTE\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\), "#", ""\\)\\)\\),""\\)/'
+done
+echo "
+Bugs"
+for t in "${bugs[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/\\2\t\\3\t=IF\\(not\\(isblank\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\)\\)\\), HYPERLINK\\(CONCAT\\("https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/",SUBSTITUTE\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\), "#", ""\\)\\)\\),""\\)/'
+done
+echo "
+Chores"
+for t in "${chores[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/\\2\t\\3\t=IF\\(not\\(isblank\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\)\\)\\), HYPERLINK\\(CONCAT\\("https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/",SUBSTITUTE\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\), "#", ""\\)\\)\\),""\\)/'
+done
+echo "
+Other"
+for t in "${other[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/\\2\t\\3\t=IF\\(not\\(isblank\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\)\\)\\), HYPERLINK\\(CONCAT\\("https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/",SUBSTITUTE\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\), "#", ""\\)\\)\\),""\\)/'
+done
+
+echo "
+
+
+--- Printing release message ---"
+echo "New draft release url: https://github.com/SFDigitalServices/sf-dahlia-lap/releases/new"
+
+echo "
+# Features"
+for t in "${features[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
+done
+echo "
+# Bug fixes"
+for t in "${bugs[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
+done
+echo "
+# Chores"
+for t in "${chores[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
+done
+echo "
+# Other"
+for t in "${other[@]}"; do
+  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
+done
