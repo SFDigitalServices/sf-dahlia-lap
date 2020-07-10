0026c939c1bcc8dd5d0c11b356b8dde1152fc8b3
James Wills <james.wills@sfgov.org>
2020-07-10 14:46:17 -0700

    simplify print script

--- print_release_info.sh
+++ print_release_info.sh
@@ -25,13 +25,28 @@ while getopts ":h::u::t:" opt; do
   esac
 done
 
+function print_section {
+  BLUE='\033[1;34m'
+  NC='\033[0m' # No Color
+  echo -e "${BLUE}
+
+
+--- $1 ---${NC}"
+}
+
+function print_error {
+  RED='\033[1;31m'
+  NC='\033[0m' # No Color
+  echo -e "${RED}$1${NC}"
+}
+
 if [ -z "$username" ]; then
-  echo "Must provide a github username."
+  print_error "Must provide a github username."
   exit
 fi
 
 if [ -z "$github_token" ]; then
-  echo "Must provide a github token. Generate a new token from Github by going to profile settings -> developer settings -> personal access token -> Generate new token."
+  print_error "Must provide a github token. Generate a new token from Github by going to profile settings -> developer settings -> personal access token -> Generate new token."
   exit
 fi
 
@@ -43,8 +58,10 @@ other=()
 commitmessages=$(git log --oneline origin/production...)
 
 numcommits=$(echo "$commitmessages" | wc -l)
+
+print_section "Commits"
 echo "$commitmessages"
-echo "Fetching the branch names for each PR for $numcommits commits"
+print_section "Fetching branch names"
 while IFS= read -r line; do
   echo "Reading $line"
   pr=$(echo "$line" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/\\4/')
@@ -70,72 +87,91 @@ while IFS= read -r line; do
 
 done <<<"$commitmessages"
 
-echo "
-
-
----- Printing google sheets output ----"
+print_section "Printing google sheets output"
 echo "Release tracker: https://docs.google.com/spreadsheets/d/1EUvw2ugaFprt8FxlCUa1yWATSn0KKo4FyRfBJWUwE3M/edit#gid=1500049656"
 
 echo "Paste the following into the release tracker on google sheets:"
 
-pr_re_full="[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$"
-pr_re_no_ticket="[a-z0-9]* (\\(.*\\) )?(.*) \\(#(.*)\\)$"
-pr_re_relaxed="[a-z0-9]* (.*)$"
+pr_re_full='[a-z0-9]* (\(.*\) )?(.*) #(.*) \(#(.*)\)$'
+pr_re_no_ticket='[a-z0-9]* (\(.*\) )?(.*) \(#(.*)\)$'
+pr_re_relaxed='[a-z0-9]* (.*)$'
 
-sheets_replacement_full='\\2\t\\3\t=IF\\(not\\(isblank\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\)\\)\\), HYPERLINK\\(CONCAT\\("https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/",SUBSTITUTE\\(INDEX\\($A$1:$C$50, ROW\\(\\), 2\\), "#", ""\\)\\)\\),""\\)\tYES\tYES\t\tNEEDS TESTING'
-sheets_replacement_no_ticket='\\2\t\tYES\tYES\t\tNEEDS TESTING'
-sheets_replacement_relaxed='\\2\t\tYES\tYES\t\tNEEDS TESTING'
-
-echo "
-Features"
-for t in "${features[@]}"; do
-  echo "$t" | sed -E "s/$pr_re_full/$sheets_replacement_full/"
-done
-echo "
-Bugs"
-for t in "${bugs[@]}"; do
-  echo "$t" | sed -E "s/$pr_re_full/$sheets_replacement_full/"
-done
-echo "
-Chores"
-for t in "${chores[@]}"; do
-  echo "$t" | sed -E "s/$pr_re_full/$sheets_replacement_full/"
-done
-echo "
-Other"
-for t in "${other[@]}"; do
-  if [[ $pr =~ $pr_re_full ]] ; then
-    echo "$t" | sed -E "s/$pr_re_full/$sheets_replacement_full/"
-  elif [[ $pr =~ $pr_re_no_ticket ]] ; then
-    echo "$t" | sed -E "s/$pr_re_no_ticket/$sheets_replacement_no_ticket/"
-  else
-    echo "$t" | sed -E "s/$pr_re_relaxed/$sheets_replacement_relaxed/"
+function print_sheet_line {
+  if [[ $1 =~ $pr_re_full ]] ; then
+    title=${BASH_REMATCH[2]}
+    ticket=${BASH_REMATCH[3]}
+  elif [[ $1 =~ $pr_re_no_ticket ]] ; then
+    title=${BASH_REMATCH[2]}
+    ticket=''
+  elif [[ $1 =~ $pr_re_relaxed ]] ; then
+    title=${BASH_REMATCH[1]}
+    ticket=''
   fi
-done
 
-echo "
+  if [ -z "$ticket" ]; then
+    ticket_link_text=''
+  else
+    ticket_link_text="https://www.pivotaltracker.com/story/show/$ticket"
+  fi
+
+  echo -e "$title\t$ticket\t$ticket_link_text\tYES\tYES\t\tNEEDS TESTING"
+}
+
+# call like `print_sheet_category <category name string> <category commit array>``
+function print_sheet_category {
+  array_name=$2[@]
+  a=("${!array_name}")
+  echo "
+$1"
+  for t in "${a[@]}"; do
+    print_sheet_line "$t"
+  done
+}
+
+print_sheet_category "Features" features
+print_sheet_category "Bugs" bugs
+print_sheet_category "Chores" chores
+print_sheet_category "Other" other
+
+function print_release_line {
+  if [[ $1 =~ $pr_re_full ]] ; then
+    title=${BASH_REMATCH[2]}
+    ticket=${BASH_REMATCH[3]}
+    pr=${BASH_REMATCH[4]}
+  elif [[ $1 =~ $pr_re_no_ticket ]] ; then
+    title=${BASH_REMATCH[2]}
+    ticket=''
+    pr=${BASH_REMATCH[3]}
+  elif [[ $1 =~ $pr_re_relaxed ]] ; then
+    title=${BASH_REMATCH[1]}
+    ticket=''
+    pr=''
+  fi
+
+  if [ -z "$ticket" ]; then
+    echo -e "- $title, (#$pr)"
+  else
+    ticket_link_text="https://www.pivotaltracker.com/story/show/$ticket"
+    echo -e "- $title [$ticket]($ticket_link_text), (#$pr)"
+  fi
+}
+
+# call like `print_release_category <category name string> <category commit array>``
+function print_release_category {
+  array_name=$2[@]
+  a=("${!array_name}")
+  echo "
+# $1"
+  for t in "${a[@]}"; do
+    print_release_line "$t"
+  done
+}
 
 
---- Printing release message ---"
+print_section "Printing release description"
 echo "New draft release url: https://github.com/SFDigitalServices/sf-dahlia-lap/releases/new"
 
-echo "
-# Features"
-for t in "${features[@]}"; do
-  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
-done
-echo "
-# Bug fixes"
-for t in "${bugs[@]}"; do
-  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
-done
-echo "
-# Chores"
-for t in "${chores[@]}"; do
-  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
-done
-echo "
-# Other"
-for t in "${other[@]}"; do
-  echo "$t" | sed -E $'s/[a-z0-9]* (\\(.*\\) )?(.*) #(.*) \\(#(.*)\\)$/- \\2 [#\\3]\\(https:\\/\\/www.pivotaltracker.com\\/story\\/show\\/\\3\\), \\(#\\4\\)/'
-done
+print_release_category "Features" features
+print_release_category "Bugs" bugs
+print_release_category "Chores" chores
+print_release_category "Other" other
