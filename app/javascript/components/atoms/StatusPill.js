1933c555e2fc31107c80aaeaf98826237a8b44d4
James Wills <james.wills@sfgov.org>
2020-08-26 14:35:40 -0700

    update snapshots

--- app/javascript/components/atoms/StatusPill.js
+++ app/javascript/components/atoms/StatusPill.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import classNames from 'classnames'
 
 import { LEASE_UP_STATUS_VALUES, getStatusPillClass, getStatusPillLabel } from '../../utils/statusUtils'
