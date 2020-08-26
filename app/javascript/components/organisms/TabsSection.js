e5dcc2e5eb350eca6aa87769b93d02f4cecc7730
Andrea Egan <andrea.egan@sfgov.org>
2020-08-26 10:23:33 -0700

    Remove unnecessary className

--- app/javascript/components/organisms/TabsSection.js
+++ app/javascript/components/organisms/TabsSection.js
@@ -5,7 +5,7 @@ import TabCard from './TabCard'
 const TabsSection = ({ children, background, padding, ...props }) => {
   return (
     <React.Fragment>
-      <div className={`tabs-section tabs-row full-width inner--3x bg-${background} stretch-to-fill-medium-down`}>
+      <div className={`tabs-section tabs-row full-width inner--3x bg-${background}`}>
         <TabsMenu {...props} />
       </div>
       <TabCard padding={padding}>
