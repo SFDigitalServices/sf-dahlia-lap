cac46052f084dde2cf70c696ab5dc6da9155acc0
James Wills <james.wills@sfgov.org>
2020-08-26 12:42:10 -0700

    add StatusPill PropTypes

--- app/javascript/components/atoms/StatusPill.js
+++ app/javascript/components/atoms/StatusPill.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import { getStatusPillClass, getStatusPillLabel } from '../../utils/statusUtils'
+import { LEASE_UP_STATUS_VALUES, getStatusPillClass, getStatusPillLabel } from '../../utils/statusUtils'
 
 const StatusPill = ({ status }) => (
   <div className={getStatusPillClass(status)}>
     <div className='pill-text'>{getStatusPillLabel(status)}</div>
   </div>
 )
+
+StatusPill.propTypes = {
+  status: PropTypes.oneOf(LEASE_UP_STATUS_VALUES)
+}
+
+StatusPill.defaultProps = {
+  status: null
+}
 
 export default StatusPill
