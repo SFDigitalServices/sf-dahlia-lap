63716c58a12ec82d8710c239a59afe605b2ce1ae
Andrea Egan <andrea.egan@sfgov.org>
2020-08-28 12:51:31 -0700

    First attempt at getting spacing right

--- app/javascript/components/molecules/ContentSection.js
+++ app/javascript/components/molecules/ContentSection.js
@@ -2,32 +2,30 @@ import React from 'react'
 import classNames from 'classnames'
 
 const ContentSection = ({ title, description, children }) => (
-  <>
+  <div className='app-inner padding-bottom--half'>
     <ContentSection.Header title={title} description={description} />
     {children}
-  </>
+  </div>
 )
 
 ContentSection.Header = ({ title, description }) => (
-  <div className='app-inner'>
+  <div>
     {title && <h2 className='app-card_h2'>{title}</h2>}
-    {description && <p className='form-note max-width'>{description}</p>}
+    {description && <p className='form-note max-width margin-bottom--3halves'>{description}</p>}
   </div>
 )
 
 ContentSection.SubHeader = ({ title, description }) => (
-  <div className='app-inner'>
+  <div>
     {title && <h3 className='app-card_h3 t-sans'>{title}</h3>}
-    {description && <p className='form-note max-width'>{description}</p>}
+    {description && <p className='form-note max-width margin-bottom--3halves'>{description}</p>}
   </div>
 )
 
-ContentSection.Content = ({ children, borderBottom, paddingBottomNone, marginTop }) => {
+ContentSection.Content = ({ children, borderBottom, marginTop }) => {
   const divClassName = classNames(
-    'app-inner',
     {
       'border-bottom': borderBottom,
-      'padding-bottom-none': paddingBottomNone,
       'margin-top': marginTop
     }
   )
