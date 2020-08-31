ecf1d0e2a16ef964d6b588566c9c540a725e48a9
Andrea Egan <andrea.egan@sfgov.org>
2020-08-31 10:17:19 -0700

    Add placeholder back to rental assistance amount

--- app/javascript/components/supplemental_application/sections/RentalAssistance.js
+++ app/javascript/components/supplemental_application/sections/RentalAssistance.js
@@ -196,6 +196,7 @@ const RentalAssistanceForm = ({ values, onSave, loading, onClose, applicationMem
               fieldName={`rental_assistances.${index}.assistance_amount`}
               validation={validateAssistanceAmount}
               id='assistance_amount'
+              placeholder='Enter Amount'
               isDirty={visited && visited[`rental_assistances.${index}.assistance_amount`]}
             />
           </FormGrid.Item>
