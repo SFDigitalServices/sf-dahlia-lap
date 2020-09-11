f3539fd646543378277d179a022e7e526bff03ff
Marcin Jedras <mjjedras@gmail.com>
2020-09-11 20:29:34 +0200

    Add tests and fixes for rental assistance delete

--- app/services/force/custom_api/rental_assistance_service.rb
+++ app/services/force/custom_api/rental_assistance_service.rb
@@ -7,6 +7,20 @@ module Force
       def destroy(id)
         @client.delete("/services/apexrest/RentalAssistance/#{id}")
       end
+
+      def delete_by(record, id)
+        result =
+          builder
+          .from(:Rental_Assistance__c)
+          .select(:Id)
+          .where_eq(record, id, :string)
+          .query
+          .records
+
+        result.each do |record|
+          destroy(record.Id)
+        end
+      end
     end
   end
 end
