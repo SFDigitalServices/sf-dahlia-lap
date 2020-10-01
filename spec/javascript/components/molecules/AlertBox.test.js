72ce38c53e24f6373728f846e05e1c6145813fc9
Anthony Kong <anthony.kong@sfgov.org>
2020-09-30 17:01:19 -0700

    separate message and click assertions

--- spec/javascript/components/molecules/AlertBox.test.js
+++ spec/javascript/components/molecules/AlertBox.test.js
@@ -3,11 +3,15 @@ import { shallow } from 'enzyme'
 import AlertBox from '~/components/molecules/AlertBox'
 
 describe('AlertBox', () => {
+  const onCloseClick = jest.fn()
+  const wrapper = shallow(<AlertBox message='This is an alert box' onCloseClick={onCloseClick} />)
+
   test('it renders an alert box with a message', () => {
-    const onCloseClick = jest.fn()
-    const wrapper = shallow(<AlertBox message='This is an alert box' onCloseClick={onCloseClick} />)
     expect(wrapper.find('.alert-body').exists()).toBeTruthy()
     expect(wrapper.find('.alert-body').text()).toEqual('This is an alert box')
+  })
+
+  test('it calls onCloseClick when the close button is clicked', () => {
     wrapper.find('.close').simulate('click')
     expect(onCloseClick).toHaveBeenCalled()
   })
