91897c3b2613659bcefb14059c27c7d1244a9cac
Anthony Kong <anthony.kong@sfgov.org>
2020-10-08 01:18:50 -0700

    combine title and content for AlertNotice test

--- spec/javascript/components/molecules/AlertNotice.test.js
+++ spec/javascript/components/molecules/AlertNotice.test.js
@@ -3,13 +3,11 @@ import { shallow } from 'enzyme'
 import AlertNotice from '~/components/molecules/AlertNotice'
 
 describe('AlertNotice', () => {
-  test('it renders an alert notice with title', () => {
-    const wrapper = shallow(<AlertNotice title='Alert notice title' />)
+  test('it renders an alert notice with title and content', () => {
+    const wrapper = shallow(
+      <AlertNotice title='Alert notice title' content='Alert notice content' />
+    )
     expect(wrapper.find('div > p:first-child').text()).toEqual('Alert notice title')
-  })
-
-  test('it renders an alert notice with content', () => {
-    const wrapper = shallow(<AlertNotice content='Alert notice content' />)
     expect(wrapper.find('div > p:last-child').text()).toEqual('Alert notice content')
   })
 
