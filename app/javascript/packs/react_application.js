36371f5387f812e5d9f3433d83dac9f67bf11f82
James Wills <james.wills@sfgov.org>
2020-10-15 16:37:15 -0700

    remove extra webpacker setup

--- app/javascript/packs/react_application.js
+++ app/javascript/packs/react_application.js
@@ -4,15 +4,11 @@ import ReactModal from 'react-modal'
 
 import ApplicationEditPage from 'components/applications/ApplicationEditPage'
 import ApplicationNewPage from 'components/applications/ApplicationNewPage'
-import ApplicationPage from 'components/applications/ApplicationPage'
 import ApplicationsPage from 'components/applications/ApplicationsPage'
 import FlaggedApplicationsIndexPage from 'components/applications/flagged/FlaggedApplicationsIndexPage'
 import FlaggedApplicationsShowPage from 'components/applications/flagged/FlaggedApplicationsShowPage'
 
 import LeaseUpApp from 'routes/LeaseUpApp'
-import LeaseUpApplicationsPage from 'components/lease_ups/LeaseUpApplicationsPage'
-import LeaseUpListingsPage from 'components/lease_ups/LeaseUpListingsPage'
-import SupplementalApplicationPage from 'components/supplemental_application/SupplementalApplicationPage'
 
 import ListingApplicationsPage from 'components/listings/ListingApplicationsPage'
 import ListingPage from 'components/listings/ListingPage'
@@ -26,18 +22,14 @@ Turbolinks.start()
 
 WebpackerReact.setup({ ApplicationEditPage }) // ES6 shorthand for {ApplicationEditPage: ApplicationEditPage}
 WebpackerReact.setup({ ApplicationNewPage })
-WebpackerReact.setup({ ApplicationPage })
 WebpackerReact.setup({ ApplicationsPage })
 WebpackerReact.setup({ FlaggedApplicationsIndexPage })
 WebpackerReact.setup({ FlaggedApplicationsShowPage })
-WebpackerReact.setup({ SupplementalApplicationPage })
 WebpackerReact.setup({ ListingApplicationsPage })
 WebpackerReact.setup({ ListingPage })
 WebpackerReact.setup({ ListingsPage })
 WebpackerReact.setup({ HomePage })
-WebpackerReact.setup({ LeaseUpApplicationsPage })
 WebpackerReact.setup({ LeaseUpApp })
-WebpackerReact.setup({ LeaseUpListingsPage })
 
 window.onload = () => {
   ReactModal.setAppElement('#root')
