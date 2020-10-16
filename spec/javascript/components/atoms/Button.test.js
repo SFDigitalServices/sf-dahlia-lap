db707ba9e36a2e30118bc4d5fa7989da611ebcc2
Anthony Kong <anthony.kong@sfgov.org>
2020-10-16 14:42:41 -0700

    address pr review comments

--- spec/javascript/components/atoms/Button.test.js
+++ spec/javascript/components/atoms/Button.test.js
@@ -6,13 +6,13 @@ describe('Button', () => {
   test('it renders a button with the correct default properties', () => {
     const wrapper = shallow(<Button text='This is a button' />)
     const selector = 'button'
-    expect(wrapper.find(selector).exists()).toBeTruthy()
-    expect(wrapper.find({ type: 'button' })).toHaveLength(1)
+    expect(wrapper.find(selector)).toHaveLength(1)
+    expect(wrapper.find(selector).props().type).toEqual('button')
     expect(wrapper.find(selector).hasClass('margin-bottom-none')).toBeFalsy()
   })
 
   test('it renders a button with the correct properties', () => {
-    const selector = 'button[type="submit"]'
+    const selector = 'button'
     const wrapper = shallow(
       <Button
         type='submit'
@@ -25,15 +25,17 @@ describe('Button', () => {
         data-other-prop='abc'
       />
     )
-    const buttonProps = wrapper.find(selector).props()
-    expect(wrapper.find(selector).exists()).toBeTruthy()
-    expect(wrapper.find(selector).text()).toEqual('This is a button')
-    expect(wrapper.find(selector).hasClass('small')).toBeTruthy()
-    expect(wrapper.find(selector).hasClass('tiny')).toBeTruthy()
-    expect(wrapper.find(selector).hasClass('tertiary')).toBeTruthy()
-    expect(wrapper.find(selector).hasClass('margin-bottom-none')).toBeTruthy()
-    expect(wrapper.find(selector).hasClass('additional-class-one')).toBeTruthy()
-    expect(wrapper.find(selector).hasClass('additional-class-two')).toBeTruthy()
-    expect(buttonProps['data-other-prop']).toMatch('abc')
+    const buttonWrapper = wrapper.find(selector)
+    const buttonProps = buttonWrapper.props()
+    expect(buttonWrapper).toHaveLength(1)
+    expect(buttonProps.type).toEqual('submit')
+    expect(buttonWrapper.text()).toEqual('This is a button')
+    expect(buttonWrapper.hasClass('small')).toBeTruthy()
+    expect(buttonWrapper.hasClass('tiny')).toBeTruthy()
+    expect(buttonWrapper.hasClass('tertiary')).toBeTruthy()
+    expect(buttonWrapper.hasClass('margin-bottom-none')).toBeTruthy()
+    expect(buttonWrapper.hasClass('additional-class-one')).toBeTruthy()
+    expect(buttonWrapper.hasClass('additional-class-two')).toBeTruthy()
+    expect(buttonProps['data-other-prop']).toEqual('abc')
   })
 })
