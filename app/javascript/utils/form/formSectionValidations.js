180d4536e656d9acc6a5c51f9d19f049c7811fca
James Wills <64036574+james-wills-sf@users.noreply.github.com>
2020-10-22 17:22:10 -0700

    Revert "Merge production fixes into main (#435)"
    This reverts commit fe7d05e4b783b7533dcfd4560a41cd6165d26159.

--- app/javascript/utils/form/formSectionValidations.js
+++ app/javascript/utils/form/formSectionValidations.js
@@ -5,16 +5,12 @@ export const isSingleRentalAssistanceValid = (form, rentalAssistanceIndex) => {
   return isEmpty(assistanceErrors) || isEmpty(assistanceErrors[rentalAssistanceIndex])
 }
 
-export const isLeaseValid = (form) => {
-  const leaseErrors = form.getState().errors.lease
-  return isEmpty(leaseErrors)
-}
-
 export const areAllRentalAssistancesValid = (form) => {
   const assistanceErrors = form.getState().errors.rental_assistances
   return isEmpty(assistanceErrors) || assistanceErrors.every(isEmpty)
 }
 
 export const areLeaseAndRentalAssistancesValid = (form) => {
-  return isLeaseValid(form) && areAllRentalAssistancesValid(form)
+  const leaseErrors = form.getState().errors.lease
+  return isEmpty(leaseErrors) && areAllRentalAssistancesValid(form)
 }
