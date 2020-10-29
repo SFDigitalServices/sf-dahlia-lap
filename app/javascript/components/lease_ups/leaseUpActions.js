4c72783ea8ba13aef7ae3d9d09b1a468c79df9a9
Andrea Egan <akegan@users.noreply.github.com>
2020-10-29 17:00:50 -0400

    Add combined applicant search functionality DAH-159 (#447)
    * Add search terms to lease up apps table SOQL query
    
    * Connect the search field to the frontend
    
    Still TODO:
    - Wire up clear X
    - Allow multiple search terms to be separated by a space
    
    * Remove unused input-clear icon
    
    * Get initial front-end styling working
    
    Need to fix the styling to make it more reusable
    
    * Use updated classes from the pattern lib to avoid hardcoding styles
    
    * Update pattern lib to 94b4901b
    
    * Add functionality to format search query as expected for API request
    
    * Update snapshots
    
    * Add tests for lease up page filters
    
    * Add backed unit tests for new search filter
    
    * Switch type of clear input button to avoid accidental clears
    
    * Use classNames for error labels across all field types, export default SearchField
    
    * Remove SearchField unused props, add proptypes, sort alphabetically
    
    * Focus search input on  onClearClick
    
    * Sanitize search strings
    
    Escapes quotes, %'s and _'s
    
    * Join individual search terms with AND instead of OR
    
    * Disable search field if modified since last submit
    
    * Strip quotes from search inputs

--- app/javascript/components/lease_ups/leaseUpActions.js
+++ app/javascript/components/lease_ups/leaseUpActions.js
@@ -5,13 +5,30 @@ import { performInSequence } from '~/utils/promiseUtils'
 
 export const getLeaseUpListings = async () => apiService.getLeaseUpListings()
 
-export const getApplications = async (listingId, page, filters) =>
-  apiService.fetchLeaseUpApplications(listingId, page, { filters }).then(({ records, pages }) => {
-    return {
-      records: map(records, buildLeaseUpAppPrefModel),
-      pages
-    }
-  })
+export const convertToCommaSeparatedList = (str) =>
+  // Take a series of phrases from the search box and turn it into a comma separated list.
+  str
+    ?.split(/ +/)
+    .filter((x) => x !== '')
+    .join(',')
+
+export const sanitizeAndFormatSearch = (str) => {
+  return convertToCommaSeparatedList(str.replace(/["']/g, ''))
+}
+
+export const getApplications = async (listingId, page, filters) => {
+  if (filters?.search) {
+    filters = { ...filters, search: sanitizeAndFormatSearch(filters?.search) }
+  }
+  return apiService
+    .fetchLeaseUpApplications(listingId, page, { filters })
+    .then(({ records, pages }) => {
+      return {
+        records: map(records, buildLeaseUpAppPrefModel),
+        pages
+      }
+    })
+}
 
 export const getListing = async (listingId) => apiService.getLeaseUpListing(listingId)
 
