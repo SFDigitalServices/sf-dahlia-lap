7b508d24a61580c8273014cdb0ebc2fbac9bf77e
Andrea Egan <andrea.egan@sfgov.org>
2020-10-29 12:00:08 -0400

    Focus search input on  onClearClick

--- app/javascript/utils/form/final_form/SearchField.js
+++ app/javascript/utils/form/final_form/SearchField.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import { Field } from 'react-final-form'
@@ -19,57 +19,65 @@ const SearchField = ({
   onClearClick,
   placeholder,
   validation
-}) => (
-  <Field
-    name={fieldName}
-    validate={validation}
-    format={format}
-    formatOnBlur={formatOnBlur && isDirty}
-  >
-    {({ input, meta }) => {
-      const hasValue = !!input.value
-      return (
-        <>
-          <div
-            className={classNames('search', {
-              'form-group': label,
-              error: meta.error && meta.touched
-            })}
-          >
-            <Label
-              label={label}
-              id={id || `form-${fieldName}`}
-              fieldName={fieldName}
-              blockNote={blockNote}
-            />
-            <input
-              {...input}
-              id={id || `form-${fieldName}`}
-              className={classNames({ 'has-value': hasValue, error: meta.error && meta.touched })}
-              type='text'
-              maxLength={maxLength}
-              placeholder={placeholder}
-              disabled={disabled}
-            />
-            {hasValue && (
-              <button
-                className='ui-medium i-aluminum button-link search-icon'
-                onClick={onClearClick}
-                type='button'
-              >
-                <svg>
-                  <use xlinkHref={`#i-close-round`} />
-                </svg>
-              </button>
-            )}
-            <FieldError meta={meta} />
-            {helpText && <HelpText note={helpText} describeId={`describe-${id || fieldName}`} />}
-          </div>
-        </>
-      )
-    }}
-  </Field>
-)
+}) => {
+  const inputElement = useRef(null)
+
+  return (
+    <Field
+      name={fieldName}
+      validate={validation}
+      format={format}
+      formatOnBlur={formatOnBlur && isDirty}
+    >
+      {({ input, meta }) => {
+        const hasValue = !!input.value
+        return (
+          <>
+            <div
+              className={classNames('search', {
+                'form-group': label,
+                error: meta.error && meta.touched
+              })}
+            >
+              <Label
+                label={label}
+                id={id || `form-${fieldName}`}
+                fieldName={fieldName}
+                blockNote={blockNote}
+              />
+              <input
+                {...input}
+                id={id || `form-${fieldName}`}
+                className={classNames({ 'has-value': hasValue, error: meta.error && meta.touched })}
+                type='text'
+                maxLength={maxLength}
+                placeholder={placeholder}
+                disabled={disabled}
+                ref={inputElement}
+              />
+              {hasValue && (
+                <button
+                  className='ui-medium i-aluminum button-link search-icon'
+                  onClick={() => {
+                    onClearClick()
+                    inputElement.current.focus()
+                  }}
+                  type='button'
+                >
+                  <svg>
+                    <use xlinkHref={`#i-close-round`} />
+                  </svg>
+                </button>
+              )}
+              <FieldError meta={meta} />
+              {helpText && <HelpText note={helpText} describeId={`describe-${id || fieldName}`} />}
+            </div>
+          </>
+        )
+      }}
+    </Field>
+  )
+}
 
 SearchField.propTypes = {
   blockNote: PropTypes.string,
