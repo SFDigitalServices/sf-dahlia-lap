0f5e5b863b62c7739e47ddb809a06814a492b34e
Andrea Egan <andrea.egan@sfgov.org>
2020-11-10 18:36:15 -0500

    Remove puts statement

--- app/controllers/api/v1/lease_up_applications_controller.rb
+++ app/controllers/api/v1/lease_up_applications_controller.rb
@@ -5,8 +5,6 @@ module Api::V1
   class LeaseUpApplicationsController < ApiController
     def index
       applications = soql_preference_service.app_preferences_for_listing(lease_up_apps_params)
-      puts 'applications from the preference service', applications[
-        :records][0].to_json
       applications[:records] = Force::Preference.convert_list(applications[:records], :from_salesforce, :to_domain)
 
       render json: applications
