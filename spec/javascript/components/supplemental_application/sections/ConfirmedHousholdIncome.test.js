d4a53606734ca173bb8eecb790cd354426d8160c
Marcin Jedras <mjjedras@gmail.com>
2021-01-06 14:43:18 +0100

    Fix unit tests

--- spec/javascript/components/supplemental_application/sections/ConfirmedHousholdIncome.test.js
+++ spec/javascript/components/supplemental_application/sections/ConfirmedHousholdIncome.test.js
@@ -85,7 +85,10 @@ describe('ConfirmedHouseholdIncome', () => {
         }
       ]
 
-      const expectedYearOptions = [{ label: 2020, value: 2020 }]
+      const expectedYearOptions = [
+        { label: 2020, value: 2020 },
+        { label: 2021, value: 2021 }
+      ]
       expect(wrapper.find('SelectField#ami_chart_type').props().options).toEqual(
         expectedChartOptions
       )
