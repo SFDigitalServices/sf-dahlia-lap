b16672cace2c2a7bfd15c81ba4611a29e74cfb27
James Wills <james.wills@sfgov.org>
2021-01-06 14:42:09 -0800

    fix logging on initial load

--- app/javascript/routes/LeaseUpApp.js
+++ app/javascript/routes/LeaseUpApp.js
@@ -1,36 +1,18 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Provider from 'context/Provider'
+import GoogleAnalyticsTracker from 'routes/GoogleAnalyticsTracker'
 
 import LeaseUpRoutes from './LeaseUpRoutes'
-
-/**
- * When the url changes, this will fire a Google Analytics pageview event.
- * Note that this component must be put inside a router but outside of any
- * routing switch components! That's because the path matches on everything,
- * so you don't want it to match and short-circuit the switch.
- */
-const GoogleAnalyticsTracker = () => (
-  <Route
-    path='/'
-    render={({ location }) => {
-      if (typeof window.ga === 'function') {
-        window.ga('set', 'page', location.pathname + location.search)
-        window.ga('send', 'pageview')
-      }
-      return null
-    }}
-  />
-)
 
 const LeaseUpApp = () => (
   <Provider>
     <Router>
       <LeaseUpRoutes />
 
-      {/* Tracker must be placed outside of LeaseUpRoutes. */}
+      {/* Tracker must be placed outside of LeaseUpRoutes' Switch component. */}
       <GoogleAnalyticsTracker />
     </Router>
   </Provider>
