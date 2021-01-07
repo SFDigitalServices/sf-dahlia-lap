e6ab4f33dc6df469b1fd0e94b5a58f423741b6d6
James Wills <james.wills@sfgov.org>
2021-01-07 12:56:05 -0800

    add comments and remove unused functions

--- app/javascript/components/supplemental_application/utils/rentalAssistanceUtils.js
+++ app/javascript/components/supplemental_application/utils/rentalAssistanceUtils.js
@@ -5,10 +5,20 @@ import { NEW_ASSISTANCE_PSEUDO_ID } from 'context/subreducers/SupplementalApplic
 export const isCreateAssistanceFormOpen = (assistanceRowsOpened) =>
   assistanceRowsOpened.has(NEW_ASSISTANCE_PSEUDO_ID)
 
+/**
+ * Given a rental assistance salesforce id, convert it to the index
+ * of that assistance on the rental assistances array.
+ */
 export const convertIdToIndex = (rentalAssistances, rentalAssistanceId) => {
   const index = findIndex(rentalAssistances, (r) => r.id === rentalAssistanceId)
   return index === -1 ? rentalAssistances.length : index
 }
 
+/**
+ * Convert rental assistance salesforce IDs to the indexes
+ * of those ids in the rentalAssistances array.
+ * @param {[RentalAssistanceObject]} rentalAssistances list of rental assistance objects
+ * @param {[String]} assistanceIds list of Salesforce IDs
+ */
 export const convertIdsToIndices = (rentalAssistances, assistanceIds) =>
   new Set([...assistanceIds].map((id) => convertIdToIndex(rentalAssistances, id)))
