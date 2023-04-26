841b503f24a8b3c6dee88c6afcae9fa0ff54b684
Chad Brokaw <chad.brokaw@exygy.com>
2023-04-26 11:27:58 -0600

    test

--- app/javascript/components/pages/HomePage.js
+++ app/javascript/components/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react'
 import PageHeader from '../organisms/PageHeader'
 
 const HomePageHeader = () => (
-  <PageHeader title='Welcome to DAHLIA Partners.' content='We hope you enjoy your stay here.' />
+  <PageHeader title='Welcome to DAHLIA Partners.' content='We hope you enjoy your stay here. :)' />
 )
 
 const HomePage = () => <HomePageHeader />
