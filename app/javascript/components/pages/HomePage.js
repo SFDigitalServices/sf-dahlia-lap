b6df1b69f38c2b709290609abfd11b39537ac5e3
Chad Brokaw <chad.brokaw@exygy.com>
2023-04-26 11:48:48 -0600

    Innocuous change

--- app/javascript/components/pages/HomePage.js
+++ app/javascript/components/pages/HomePage.js
@@ -3,7 +3,10 @@ import React from 'react'
 import PageHeader from '../organisms/PageHeader'
 
 const HomePageHeader = () => (
-  <PageHeader title='Welcome to DAHLIA Partners.' content='We hope you enjoy your stay here.' />
+  <PageHeader
+    title='Welcome to DAHLIA Partners.'
+    content='We hope you enjoy your stay here. ROLLBACK'
+  />
 )
 
 const HomePage = () => <HomePageHeader />
