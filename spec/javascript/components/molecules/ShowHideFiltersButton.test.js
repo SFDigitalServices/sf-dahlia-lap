425304fb65a9d3bd9cd1aed004b18846c2d8ed71
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2023-11-09 12:50:13 -0700

    More tests

--- spec/javascript/components/molecules/ShowHideFiltersButton.test.js
+++ spec/javascript/components/molecules/ShowHideFiltersButton.test.js
@@ -1,32 +1,28 @@
 import React from 'react'
 
-import { shallow } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 
-import Button from 'components/atoms/Button'
-import StyledIcon from 'components/atoms/StyledIcon'
 import ShowHideFiltersButton from 'components/molecules/ShowHideFiltersButton'
 
 describe('ShowHideFiltersButton', () => {
   test('it renders properly with default props', () => {
-    const wrapper = shallow(<ShowHideFiltersButton />)
-    expect(wrapper.find(Button).props().text).toEqual('Show Filters')
-    expect(wrapper.find(Button).props().iconRight).toBeNull()
-    expect(wrapper.find(Button).props().iconLeft).not.toBeNull()
-    expect(wrapper.find(Button).props().minWidthPx).toEqual('195px')
+    render(<ShowHideFiltersButton />)
+    expect(screen.getByText('Show Filters')).toBeInTheDocument()
+    screen.logTestingPlaygroundURL()
+    expect(screen.queryByTestId('button-iconRight')).not.toBeInTheDocument()
+    expect(screen.getByTestId('button-iconLeft')).toBeInTheDocument()
   })
 
   test('it renders properly when isShowingFilters=true', () => {
-    const wrapper = shallow(<ShowHideFiltersButton isShowingFilters />)
-    expect(wrapper.find(Button).props().text).toEqual('Hide Filters')
-    expect(wrapper.find(Button).props().iconRight).toBeNull()
-    expect(wrapper.find(Button).props().iconLeft).not.toBeNull()
-    expect(wrapper.find(Button).props().minWidthPx).toEqual('195px')
+    render(<ShowHideFiltersButton isShowingFilters />)
+    expect(screen.getByText('Hide Filters')).toBeInTheDocument()
+    expect(screen.queryByTestId('button-iconRight')).not.toBeInTheDocument()
+    expect(screen.getByTestId('button-iconLeft')).toBeInTheDocument()
   })
 
   test('it renders properly when numFiltersApplied=4', () => {
-    const wrapper = shallow(<ShowHideFiltersButton isShowingFilters numFiltersApplied={4} />)
-    expect(wrapper.find(Button).props().iconRight).toEqual(<StyledIcon icon='filter-qty--4' />)
-    expect(wrapper.find(Button).props().iconLeft).not.toBeNull()
-    expect(wrapper.find(Button).props().minWidthPx).toEqual('236px')
+    const { asFragment } = render(<ShowHideFiltersButton isShowingFilters numFiltersApplied={4} />)
+    expect(screen.getByTestId('button-iconLeft')).toBeInTheDocument()
+    expect(asFragment()).toMatchSnapshot()
   })
 })
