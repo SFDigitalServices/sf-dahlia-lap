3b5295c174a90f09b0fd9b966bcba1f3e5771279
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-02-19 11:04:12 -0800

    Revert "add error test"
    This reverts commit 9f79a0d500e6f0f242ba37c4e09403cddfdb5f27.

--- app/javascript/components/pages/HomePage.js
+++ app/javascript/components/pages/HomePage.js
@@ -4,13 +4,8 @@ import ErrorBoundary from 'components/atoms/ErrorBoundary'
 
 import PageHeader from '../organisms/PageHeader'
 
-const ErrorTest = () => {
-  throw new Error('error test')
-}
-
 const HomePageHeader = () => (
   <ErrorBoundary>
-    <ErrorTest />
     <PageHeader title='Welcome to DAHLIA Partners.' content='We hope you enjoy your stay here.' />
   </ErrorBoundary>
 )
