792923e187768f110fb1bd6519569bb26adb93c1
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2024-02-21 15:21:08 -0700

    Suppress and validate Error Boundary test output

--- spec/javascript/components/atoms/ErrorBoundary.test.js
+++ spec/javascript/components/atoms/ErrorBoundary.test.js
@@ -26,10 +26,16 @@ describe('ErrorBoundary', () => {
         <div>{content}</div>
       </ErrorBoundary>
     )
-    getByText(content)
+    expect(getByText(content)).toBeInTheDocument()
   })
 
   it('displays the error message when there is an error', async () => {
+    // The goal of this test is to ensure that the ErrorBoundary component works correctly
+    // We want to capture the error and assert that it is the correct one
+    // but we don't want to see the error output in the console
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+    jest.spyOn(console, 'debug').mockImplementation(() => {})
+
     const { getByText } = render(
       <ErrorBoundary>
         <div>
@@ -37,6 +43,11 @@ describe('ErrorBoundary', () => {
         </div>
       </ErrorBoundary>
     )
-    getByText('An error occurred. Check back later.')
+
+    expect(consoleSpy).toHaveBeenCalledTimes(3)
+    expect(consoleSpy.mock.calls[0][0].toString()).toBe(
+      'Error: Uncaught [Error: This is an error to test ErrorBoundary]'
+    )
+    expect(getByText('An error occurred. Check back later.')).toBeInTheDocument()
   })
 })
