007485dcc78225acc88b5d1edde3b65da714f4d6
tallulah <tallulah.kay@gmail.com>
2024-04-30 17:44:43 -0700

    DAH-2293 add comments and tests for fcfs api call

--- app/controllers/api/v1/lease_up_applications_controller.rb
+++ app/controllers/api/v1/lease_up_applications_controller.rb
@@ -5,13 +5,16 @@ module Api::V1
   class LeaseUpApplicationsController < ApiController
     def index
       if listing_type == Force::Listing::LISTING_TYPE_FIRST_COME_FIRST_SERVED
+        # First Come, First Served listings don't have preferences
         applications = soql_lease_up_application_service.lease_up_applications(lease_up_apps_params)
         applications[:records] = Force::Application.convert_list(applications[:records], :from_salesforce, :to_domain)
       else
+        # All other listings need to be queried by preferences first
         applications = soql_preference_service.app_preferences_for_listing(lease_up_apps_params)
         applications[:records] = Force::Preference.convert_list(applications[:records], :from_salesforce, :to_domain)
       end
 
+      # providing the listing type so we know how to handle the response
       applications[:listing_type] = listing_type
       render json: applications
     end
