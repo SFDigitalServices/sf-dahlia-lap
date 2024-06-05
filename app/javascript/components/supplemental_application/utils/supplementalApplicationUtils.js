ee79b51921f6043da252402df826a266683ed0da
Tallulah Kay <43218114+tallulahkay@users.noreply.github.com>
2024-06-05 09:57:31 -0700

    DAH-2293 check for preferences existence (#638)
    * DAH-2293 check for preferences existence
    
    * DAH-2293 use chaining

--- app/javascript/components/supplemental_application/utils/supplementalApplicationUtils.js
+++ app/javascript/components/supplemental_application/utils/supplementalApplicationUtils.js
@@ -5,9 +5,11 @@ export const isLeaseAlreadyCreated = (lease) => lease && !!lease.id
 export const doesApplicationHaveLease = (application) => isLeaseAlreadyCreated(application?.lease)
 
 export const totalSetAsidesForPref = (listing, prefName) =>
-  listing.listing_lottery_preferences.find((pref) =>
-    pref.lottery_preference.name.includes(prefName)
-  )?.available_units ?? 0
+  listing.listing_lottery_preferences
+    ? listing.listing_lottery_preferences.find((pref) =>
+        pref.lottery_preference.name.includes(prefName)
+      )?.available_units ?? 0
+    : 0
 
 export const getApplicationMembers = (application) => [
   ...([application?.applicant] || []),
