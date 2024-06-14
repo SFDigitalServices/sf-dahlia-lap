735438138318126efb14febc9545b0a81daf8185
tallulah <tallulah.kay@gmail.com>
2024-06-14 16:51:35 -0700

    DAH-2334 remove unneeded changes

--- app/javascript/routes/LeaseUpRoutes.js
+++ app/javascript/routes/LeaseUpRoutes.js
@@ -5,7 +5,6 @@ import { Routes, Route } from 'react-router-dom'
 import ApplicationPage from 'components/applications/ApplicationPage'
 import LeaseUpApplicationsPage from 'components/lease_ups/LeaseUpApplicationsPage'
 import LeaseUpListingsPage from 'components/lease_ups/LeaseUpListingsPage'
-import LotteryResultsPdfGenerator from 'components/lease_ups/lottery_results_page/LotteryResultsPdfGenerator'
 import SupplementalApplicationPage from 'components/supplemental_application/SupplementalApplicationPage'
 import appPaths from 'utils/appPaths'
 
@@ -35,11 +34,6 @@ const LeaseUpRoutes = () => (
       element={<SupplementalApplicationPage />}
     />
     <Route exact path={appPaths.toApplication(':applicationId')} element={<ApplicationPage />} />
-    <Route
-      exact
-      path={appPaths.toLotteryResults(':listingId')}
-      element={<LotteryResultsPdfGenerator />}
-    />
   </Routes>
 )
 
