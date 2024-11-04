c079e9334fb5c194cd172c777ae42aec540b65da
tallulah <tallulah.kay@gmail.com>
2024-11-04 12:50:05 -0800

    fix: DAH-2915 redirect when logged out

--- lib/salesforce_logout_app.rb
+++ lib/salesforce_logout_app.rb
@@ -6,8 +6,7 @@ class SalesforceLogoutApp < Devise::FailureApp
     store_location!
     message = warden.message || warden_options[:message]
 
-    # For some reason it also needs to be called when message is nil
-    if message.nil? || message == :timeout
+    if message == :timeout
       @salesforce_logout_host = warden.params[:admin] ? ENV['SALESFORCE_INSTANCE_URL'] : ENV['COMMUNITY_LOGIN_URL']
       redirect_to "#{@salesforce_logout_host}/secur/logout.jsp" and return
     else
