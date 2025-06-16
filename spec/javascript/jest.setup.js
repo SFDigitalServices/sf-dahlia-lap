c7e36087307ebb5ed0e2001ec9facc338c4473bd
josh-chou <626661+josh-chou@users.noreply.github.com>
2025-06-16 14:35:39 -0700

    chore: DAH-3449 partners unit test console errors (#737)
    * chore: install jest-fail-on-console
    
    * chore: suppress console logs during testing

--- spec/javascript/jest.setup.js
+++ spec/javascript/jest.setup.js
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom'
+import failOnConsole from 'jest-fail-on-console'
 
 // Make vars from the .env file available in process.env
 const dotenv = require('dotenv')
@@ -7,3 +8,10 @@ dotenv.config()
 window.scrollTo = jest.fn()
 
 global.wait = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms))
+
+// suppress console logs during testing
+failOnConsole({
+  silenceMessage: (errorMessage) => {
+    return true
+  }
+})
