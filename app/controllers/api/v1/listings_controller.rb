bbd814854b8fb4d4cde356e0b2eb1b8cb19ec655
Josh Chou <626661+josh-chou@users.noreply.github.com>
2025-10-08 13:19:23 -0700

    fix: run qlty fmt

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     # RESTful JSON API for listings actions
     class ListingsController < ApiController
-        before_action :authenticate_user!
+      before_action :authenticate_user!
 
       def update
         response = rest_listing_service.update(listing_params.merge(id: params[:id]))
