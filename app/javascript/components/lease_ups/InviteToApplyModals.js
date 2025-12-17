8fd8f4baa5b61bc117d3457ceda5e2e157562fd1
josh-chou <626661+josh-chou@users.noreply.github.com>
2025-12-16 16:53:38 -0800

    fix: DAH-3808 fix send an example UI issues (#775)
    * chore: disable send yourself an example link
    
    * test: comment out tests for example link
    
    * chore: re-enable send example link
    
    * fix: email validation should fail on empty string
    
    * fix: remove cancel link
    
    * fix: toggle functionality of main submit button in example email modal
    
    * test: update frontend tests
    
    * test: fix e2e tests

--- app/javascript/components/lease_ups/InviteToApplyModals.js
+++ app/javascript/components/lease_ups/InviteToApplyModals.js
@@ -268,7 +268,8 @@ export const InviteToApplyModals = forwardRef((props, ref) => {
           <div className={'form-group'}>
             <p>
               When you’re ready, send an email to the applicants you selected. If you want,&nbsp;
-              <a>send yourself an example email</a> to preview what applicants will see.
+              <a onClick={() => showRsvpModal('example')}>send yourself an example email</a> to
+              preview what applicants will see.
             </p>
             <p>
               <label className='form-label'>You are sending</label>
@@ -309,11 +310,9 @@ export const InviteToApplyModals = forwardRef((props, ref) => {
       </FormModal>
       <FormModal
         isOpen={rsvpModalState.example}
-        onSubmit={sendInviteToApply}
+        onSubmit={exampleSuccessAlertState.show ? closeExampleModal : sendInviteToApply}
         handleClose={closeExampleModal}
-        primary='send example email'
-        secondary='cancel'
-        onSecondaryClick={closeExampleModal}
+        primary={exampleSuccessAlertState.show ? 'done' : 'send example email'}
       >
         {() => (
           <div className={'form-group'}>
@@ -344,7 +343,9 @@ export const InviteToApplyModals = forwardRef((props, ref) => {
                   rows='10'
                   ariaDescribedby='invite-to-apply-example-label'
                   maxLength='255'
-                  validation={validate.isValidEmail('Enter email address like: example@web.com')}
+                  validation={validate.isValidEmailStrict(
+                    'Enter email address like: example@web.com'
+                  )}
                 />
               </FormGrid.Item>
             </FormGrid.Row>
--- app/javascript/utils/form/validations.js
+++ app/javascript/utils/form/validations.js
@@ -75,6 +75,11 @@ const isValidEmail = (email) => {
 
 const isEmptyString = (value) => isNil(value) || value.length === 0
 
+const isValidEmailStrict = (email) => {
+  // does not allow for empty string
+  return isEmptyString(email) ? false : isValidEmail(email)
+}
+
 export const isValidCurrency = (value) => {
   if (isEmptyString(value)) {
     return true
@@ -125,6 +130,7 @@ const isValidUrl = (value) => {
 }
 
 validate.isValidEmail = decorateValidator(isValidEmail)
+validate.isValidEmailStrict = decorateValidator(isValidEmailStrict)
 validate.isOldEnough = decorateValidator(isOldEnough)
 validate.isDate = decorateValidator(isDate)
 validate.isFutureDate = decorateValidator(isFutureDate)
