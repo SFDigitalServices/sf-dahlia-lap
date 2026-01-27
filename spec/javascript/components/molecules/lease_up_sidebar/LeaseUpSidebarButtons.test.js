b371300541a90ed79cd04a36d819f404f3e0d8f7
cliu02 <32694266+cliu02@users.noreply.github.com>
2026-01-27 13:44:31 -0800

    test: update test

--- spec/javascript/components/molecules/lease_up_sidebar/LeaseUpSidebarButtons.test.js
+++ spec/javascript/components/molecules/lease_up_sidebar/LeaseUpSidebarButtons.test.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 
 import LeaseUpStatusButtons from 'components/molecules/lease_up_sidebar/LeaseUpStatusButtons'
+import { LEASE_UP_STATUS_OPTIONS } from 'utils/statusUtils'
 
 const ON_CHANGE_STATUS = jest.fn()
 
@@ -32,25 +33,49 @@ jest.mock('react-select', () => (props) => {
 
 describe('LeaseUpStatusButtons', () => {
   test('should render the status dropdown correctly', () => {
-    render(<LeaseUpStatusButtons onChangeStatus={ON_CHANGE_STATUS} status={'Approved'} />)
+    render(
+      <LeaseUpStatusButtons
+        onChangeStatus={ON_CHANGE_STATUS}
+        status={'Approved'}
+        statusOptions={LEASE_UP_STATUS_OPTIONS}
+      />
+    )
     expect(screen.getByRole('combobox')).toBeInTheDocument()
     expect(screen.getByRole('combobox')).toHaveValue('Approved')
   })
 
   test('should render the status dropdown correctly when no status is passed', () => {
-    render(<LeaseUpStatusButtons onChangeStatus={ON_CHANGE_STATUS} />)
+    render(
+      <LeaseUpStatusButtons
+        onChangeStatus={ON_CHANGE_STATUS}
+        statusOptions={LEASE_UP_STATUS_OPTIONS}
+      />
+    )
     expect(screen.getByRole('combobox')).toBeInTheDocument()
     expect(screen.getByRole('combobox')).toHaveValue('')
   })
 
   test('should render a comment button', () => {
-    render(<LeaseUpStatusButtons onChangeStatus={ON_CHANGE_STATUS} status={'Approved'} />)
+    render(
+      <LeaseUpStatusButtons
+        onChangeStatus={ON_CHANGE_STATUS}
+        status={'Approved'}
+        statusOptions={LEASE_UP_STATUS_OPTIONS}
+      />
+    )
     expect(screen.getByRole('button')).toBeInTheDocument()
     expect(screen.getByRole('button')).toBeEnabled()
   })
 
   test('should disable both buttons when loading', () => {
-    render(<LeaseUpStatusButtons onChangeStatus={ON_CHANGE_STATUS} status={'Approved'} isLoading />)
+    render(
+      <LeaseUpStatusButtons
+        onChangeStatus={ON_CHANGE_STATUS}
+        status={'Approved'}
+        isLoading
+        statusOptions={LEASE_UP_STATUS_OPTIONS}
+      />
+    )
 
     expect(screen.getByRole('button')).toBeDisabled()
     expect(screen.getByRole('combobox')).toHaveAttribute('data-disabled', 'true')
