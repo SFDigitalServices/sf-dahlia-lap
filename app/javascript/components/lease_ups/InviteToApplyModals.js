854de1a9c8f0ae81670e3a8786a9d98567648a5d
Josh Chou <626661+josh-chou@users.noreply.github.com>
2026-03-06 13:34:53 -0800

    fix: uncheck selected checkboxes after sending i2a

--- app/javascript/components/lease_ups/InviteToApplyModals.js
+++ app/javascript/components/lease_ups/InviteToApplyModals.js
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle } from 'react'
+import React, { forwardRef, useEffect, useImperativeHandle } from 'react'
 
 import arrayMutators from 'final-form-arrays'
 import { map } from 'lodash'
@@ -239,6 +239,22 @@ export const InviteToApplyModals = forwardRef((props, ref) => {
       (app) => !app.email && selectedIds.includes(app.application_id)
     ).length
   }
+
+  const uncheckSelectedApps = () => {
+    const selectedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked')
+    selectedCheckboxes.forEach((checkbox) => {
+      if (checkbox.id.includes('bulk-action-checkbox')) {
+        checkbox.checked = false
+      }
+    })
+  }
+
+  useEffect(() => {
+    // uncheck any selected checkboxes when loading is finished to reset the page state after sending invites
+    if (props.pageState.loading === false) {
+      uncheckSelectedApps()
+    }
+  }, [props.pageState.loading])
 
   const sendInviteToApply = async (submittedValues) => {
     const appIds = getSelectedApplicationIds()
