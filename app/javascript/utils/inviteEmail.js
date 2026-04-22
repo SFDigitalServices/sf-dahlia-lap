026a5135b1285442f163f123505d813326a39f8f
Josh Chou <626661+josh-chou@users.noreply.github.com>
2026-04-22 13:42:11 -0700

    feat: use Leaseup_Outreach field to determine i2x

--- app/javascript/utils/inviteEmail.js
+++ app/javascript/utils/inviteEmail.js
@@ -1,4 +1,3 @@
-import { PROGRAM_TYPE_INCLUSIONARY_RENTAL, LISTING_TYPE_FIRST_COME_FIRST_SERVED } from './consts'
 import {
   LEASE_UP_SUBSTATUS_OPTIONS,
   LEASE_UP_SUBSTATUSES,
@@ -9,30 +8,15 @@ import {
 export const I2A_FEATURE_FLAG = 'partners.inviteToApply'
 export const I2I_FEATURE_FLAG = 'all.i2i'
 
+export const I2I_OUTREACH_VALUE = 'Appointments required'
+export const I2A_OUTREACH_VALUE = 'Submit all info online'
+
 export const IsInviteToApplyEnabledForListing = (listing, i2aFlag) => {
-  return (
-    i2aFlag &&
-    listing &&
-    listing.program_type === PROGRAM_TYPE_INCLUSIONARY_RENTAL &&
-    listing.listing_type !== LISTING_TYPE_FIRST_COME_FIRST_SERVED
-  )
+  return i2aFlag && listing && listing.leaseup_outreach === I2A_OUTREACH_VALUE
 }
 
-export const IsI2IEnabledForListing = (listing, i2iFlag, variant) => {
-  let enabledIds = []
-  const payloadValue = variant?.payload?.value
-
-  if (payloadValue) {
-    try {
-      const parsedPayload = JSON.parse(payloadValue)
-      enabledIds = Array.isArray(parsedPayload.enabled_listings)
-        ? parsedPayload.enabled_listings
-        : []
-    } catch (error) {
-      enabledIds = []
-    }
-  }
-  return i2iFlag && listing && enabledIds.includes(listing.id)
+export const IsI2IEnabledForListing = (listing, i2iFlag, _variant) => {
+  return i2iFlag && listing && listing.leaseup_outreach === I2I_OUTREACH_VALUE
 }
 
 export const INVITE_EMAIL_OPTIONS = [
